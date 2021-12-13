2ab8695d3a9a40510bb6348c9708135d8c074d0c
Victor van Duijnen <victor.van.duijnen@smart.coop>
2021-12-13 10:52:32 +0100

    Add DesignTimeContextFactory
    It allows self-contained db settings in the infrastructure layer

---
+++ src/Smart.FA.Catalog.Infrastructure/DesignTimeContextFactory.cs
@@ -0,0 +1,42 @@
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure;
+public class DesignTimeContextFactory : IDesignTimeDbContextFactory<Context>
+{
+    public Context CreateDbContext(string[] args)
+    {
+        var basePath = Directory.GetCurrentDirectory();
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        Console.WriteLine($"\nDesignTimeContextFactory.Create(string[]):\n\tBase Path: {basePath}\n\tEnvironmentVariable: {environmentName}");
+        return Create(basePath, environmentName);
+    }
+
+    private static Context Create(string basePath, string environmentName)
+    {
+        const string appSettingsFileName = "appsettings";
+        var builder = new ConfigurationBuilder()
+                     .SetBasePath(basePath)
+                     .AddJsonFile($"{appSettingsFileName}.json", false)
+                     .AddJsonFile($"{appSettingsFileName}.{environmentName}.json", true)
+                     .AddJsonFile($"{appSettingsFileName}.Local.json", true)
+                     .AddEnvironmentVariables();
+
+        var config = builder.Build();
+        var connectionString = config.GetConnectionString("Training");
+
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new InvalidOperationException(
+                "Could not find a connection string named 'Training'.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<Context>();
+        Console.WriteLine($"Setting provider");
+
+        optionsBuilder.UseSqlServer(connectionString);
+        Console.WriteLine($"\nDesignTimeContextFactory.Create(string):\n\tConnection string: {connectionString}\n");
+        var options = optionsBuilder.Options;
+        return new Context(options);
+    }
+}
