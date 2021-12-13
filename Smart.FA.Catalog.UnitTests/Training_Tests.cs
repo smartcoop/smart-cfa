05005d4b9e8df24f1bf8d1fd8f17125f227d94b0
Victor van Duijnen <victor.van.duijnen@smart.coop>
2021-12-13 11:05:00 +0100

    Add boiler code for the training unit tests

---
+++ Smart.FA.Catalog.UnitTests/Training_Tests.cs
@@ -0,0 +1,61 @@
+using Xunit;
+
+namespace Smart.FA.Catalog.UnitTests;
+
+public class TrainingTests
+{
+
+    [Fact]
+    public void TrainingHasInitiallyNoTrainers()
+    {
+
+    }
+    [Fact]
+    public void CanAddTrainer()
+    {
+
+    }
+
+    [Fact]
+    public void CanAddTrainers()
+    {
+
+    }
+
+    [Fact]
+    public void CanUpdateDescription()
+    {
+    }
+
+    [Fact]
+    public void MustBeAutoValidated()
+    {
+    }
+    [Fact]
+    public void MustBeManuallyValidated()
+    {
+
+    }
+
+    [Fact]
+    public void HasValidGoalLength()
+    {
+
+    }
+
+    [Fact]
+    public void HasValidTrainingType()
+    {
+
+    }
+
+    [Fact]
+    public void HasValidTargetAudience()
+    {
+    }
+
+    [Fact]
+    public void CanBeSwitchedToPrivateTraining() { }
+    [Fact]
+    public void CanBeSwitchedToCollectiveTraining() { }
+}
