72e3d4cfcc1657be4c8072f0c36a7338d71c96a5
Victor van Duijnen <victor.van.duijnen@smart.coop>
2021-12-13 10:30:28 +0100

    Add basic response type for usecases
    Template for base response from the use case in the application layer

---
+++ src/Smart.FA.Catalog.Application/SeedWork/ResponseBase.cs
@@ -0,0 +1,74 @@
+// Application
+
+using Core.SeedWork;
+
+namespace Application.SeedWork;
+
+/// <summary>
+/// Base response for Mediatr handlers
+/// </summary>
+public class ResponseBase
+{
+    private readonly List<ApplicationError> _errors;
+    public bool IsSuccess { get; private set; }
+    public IReadOnlyCollection<ApplicationError> Errors => _errors;
+
+    /// <summary>
+    /// Creates a new response base
+    /// By default, it contains no errors and is set to success = false
+    /// </summary>
+    /// <param name="success"></param>
+    protected ResponseBase()
+    {
+        _errors = new List<ApplicationError>();
+    }
+
+    /// <summary>
+    /// Adds a string error to the response
+    /// </summary>
+    /// <param name="message">The error message</param>
+    public void AddError(string code, string message)
+    {
+        IsSuccess = false;
+        _errors.Add(new ApplicationError(code, message));
+    }
+
+    /// <summary>
+    /// Adds string error to the response
+    /// </summary>
+    /// <param name="exception">The exception</param>
+    public void AddError(DomainException exception)
+    {
+        AddError(exception.Code, exception.Message);
+    }
+
+    /// <summary>
+    /// Removes all error messages and sets success to true
+    /// </summary>
+    public void SetSuccess()
+    {
+        IsSuccess = true;
+        _errors.Clear();
+    }
+
+    /// <summary>
+    /// Check if response contains any errors
+    /// </summary>
+    /// <returns>True if more than 0 errors</returns>
+    public bool HasErrors()
+    {
+        return _errors.Count > 0;
+    }
+}
+
+public class ApplicationError
+{
+    public string Code { get; set; }
+    public string Message { get; set; }
+
+    public ApplicationError(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+}
