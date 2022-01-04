bb89327330220102ace34c69319d577472416c75
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-04 13:02:37 +0100

    Add use case for fetching trainers from a training id

---
+++ src/Smart.FA.Catalog.Application/UseCases/Queries/GetTrainersFromTrainingQueryHandler.cs
@@ -0,0 +1,47 @@
+using Application.SeedWork;
+using Core.Domain;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Application.UseCases.Queries;
+
+public class GetTrainersFromTrainingQueryHandler : IRequestHandler<GetTrainersFromTrainingRequest, GetTrainersFromTrainingResponse>
+{
+    private readonly ILogger<GetTrainersFromTrainingQueryHandler> _logger;
+    private readonly Context _context;
+
+    public GetTrainersFromTrainingQueryHandler(ILogger<GetTrainersFromTrainingQueryHandler> logger, Context context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    public async Task<GetTrainersFromTrainingResponse> Handle(GetTrainersFromTrainingRequest request, CancellationToken cancellationToken)
+    {
+        GetTrainersFromTrainingResponse response = new();
+        try
+        {
+            var training = await _context.Trainings.FirstOrDefaultAsync(training => training.Id == request.TrainingId);
+            response.Trainers = training?.TrainerEnrollments.Select(ttt => ttt.Trainer).ToList();
+            response.SetSuccess();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.StackTrace);
+            throw;
+        }
+
+        return response;
+    }
+}
+
+public class GetTrainersFromTrainingRequest: IRequest<GetTrainersFromTrainingResponse>
+{
+    public int TrainingId { get; set; }
+}
+
+public class GetTrainersFromTrainingResponse: ResponseBase
+{
+    public ICollection<Trainer>? Trainers { get; set; }
+}
