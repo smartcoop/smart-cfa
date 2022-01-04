a0f1ec09617bd7166efe50e5c167fbd943fc9b88
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-04 12:56:36 +0100

    Add CreateClean method to factory training/trainer

--- Smart.FA.Catalog.Tests.Common/TrainerFactory.cs
+++ Smart.FA.Catalog.Tests.Common/TrainerFactory.cs
@@ -5,9 +5,15 @@ namespace Smart.FA.Catalog.Tests.Common;
 
 public class TrainerFactory
 {
-    public Trainer Create()
+    public Trainer CreateClean()
     {
         var fixture = new Fixture();
-        return new Trainer(fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>());
+        return new Trainer(fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>().Substring(0,2));
     }
+    public Trainer Create(string firstName, string lastName)
+    {
+        var fixture = new Fixture();
+        return new Trainer(firstName, lastName, fixture.Create<string>(), fixture.Create<string>().Substring(0,2));
+    }
+
 }
--- Smart.FA.Catalog.Tests.Common/TrainingFactory.cs
+++ Smart.FA.Catalog.Tests.Common/TrainingFactory.cs
@@ -8,14 +8,18 @@ public class TrainingFactory
     public Training Create(Trainer trainer)
     {
         Fixture fixture = new();
-       return new Training(trainer, new List<TrainingType> {TrainingType.Other, TrainingType.Professional},
+
+       var training = new Training(trainer, new List<TrainingType> {TrainingType.SchoolCourse, TrainingType.Professional},
             TrainingSlotNumberType.Single,
             new List<TrainingTargetAudience> {TrainingTargetAudience.Employee, TrainingTargetAudience.Student});
+       return training;
     }
 
     public Training CreateClean()
     {
         TrainerFactory trainerFactory = new();
-        return Create(trainerFactory.Create());
+        return Create(trainerFactory.CreateClean());
     }
+
+
 }
