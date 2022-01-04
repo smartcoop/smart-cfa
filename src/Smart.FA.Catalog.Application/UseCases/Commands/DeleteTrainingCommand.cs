2906a41149f87bf2ad84c65badc3bcb1597dd146
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-04 13:02:09 +0100

    Add use case for deleting a training

---
+++ src/Smart.FA.Catalog.Application/UseCases/Commands/DeleteTrainingCommand.cs
@@ -0,0 +1,49 @@
+using Application.SeedWork;
+using Core.SeedWork;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.UseCases.Commands;
+
+public class DeleteTrainingCommand: IRequestHandler<DeleteTrainingRequest, DeleteTrainingResponse>
+{
+    private readonly ILogger<DeleteTrainingCommand> _logger;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly Context _context;
+
+    public DeleteTrainingCommand(ILogger<DeleteTrainingCommand> logger, IUnitOfWork unitOfWork, Context context)
+    {
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+        _context = context;
+    }
+    public async Task<DeleteTrainingResponse> Handle(DeleteTrainingRequest request, CancellationToken cancellationToken)
+    {
+        DeleteTrainingResponse resp = new();
+
+        try
+        {
+            var training = await _context.Trainings.FindAsync(request.TrainingId);
+            _unitOfWork.RegisterDeleted(training);
+            _unitOfWork.Commit();
+            resp.SetSuccess();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.StackTrace);
+            throw;
+        }
+       return resp;
+    }
+}
+
+public class DeleteTrainingRequest: IRequest<DeleteTrainingResponse>
+{
+    public int TrainingId { get; set; }
+}
+
+public class DeleteTrainingResponse : ResponseBase
+{
+
+}
