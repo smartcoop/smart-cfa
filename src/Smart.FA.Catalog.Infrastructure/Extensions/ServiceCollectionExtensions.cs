6fe0ba9ec93804aaa0c9dba230e16ba83826aba5
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-07 11:42:22 +0100

    Add dependency injection for queries and repositories

--- src/Smart.FA.Catalog.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Smart.FA.Catalog.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,21 +1,54 @@
+using Core.Domain.Interfaces;
+using Core.SeedWork;
+using Core.Services;
 using Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+using Infrastructure.Persistence.Database;
+using Infrastructure.Persistence.Read;
+using Infrastructure.Persistence.Write;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddInfrastructure(this IServiceCollection services, string connectionString)
+    public static void AddInfrastructure(this IServiceCollection services, string connectionString, bool useConsoleLogger, IConfigurationSection mailOptionSection)
     {
-        services.AddContext(connectionString);
+        services.AddContext(connectionString, useConsoleLogger)
+            .AddRepositories()
+            .AddServices(mailOptionSection)
+            .AddQueries(connectionString);
     }
 
-    private static IServiceCollection AddContext(this IServiceCollection services, string connectionString)
+    private static IServiceCollection AddContext(this IServiceCollection services, string connectionString, bool useConsoleLogger)
     {
-        services.AddDbContext<Context>(options => options
-                                             .UseSqlServer(connectionString))
-                                             .BuildServiceProvider();
+        services.AddScoped(_ => new Context(connectionString, useConsoleLogger));
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfigurationSection mailOptionSection)
+    {
+        services.Configure< MailOptions >( mailOptionSection );
+        services.AddScoped<IMailService, MailService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddQueries(this IServiceCollection services, string connectionString)
+    {
+        services.AddScoped<ITrainerQueries>(_ => new TrainerQueries(connectionString));
+        services.AddScoped<ITrainingQueries>(_ => new TrainingQueries(connectionString));
+
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ITrainingRepository, TrainingRepository>();
+        services.AddScoped<ITrainerRepository, TrainerRepository>();
+
         return services;
     }
 }
