c1540bc06ba913a3db28a0c166f6e726575f32f6
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-07 11:36:52 +0100

    Add guard clause helper methods
    Clean syntactic sugar for guard clause

---
+++ src/Smart.FA.Catalog.Core/SeedWork/Guard.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Core.Exceptions;
+
+namespace Core.SeedWork;
+
+public static class Guard
+{
+    public static void Requires(Func<bool> predicate, string message)
+    {
+        if (predicate()) return;
+        Debug.Fail(message);
+        throw new GuardClauseException(message);
+    }
+
+    [Conditional("DEBUG")]
+    public static void Ensures(Func<bool> predicate, string message)
+    {
+        Debug.Assert(predicate(), message);
+    }
+
+    public static void AgainstNull(object argumentValue, string argumentName)
+    {
+        if (argumentValue == null)
+            throw new ArgumentNullException(argumentName);
+    }
+}
