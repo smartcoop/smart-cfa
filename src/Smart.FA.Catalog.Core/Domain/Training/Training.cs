847f5e0c3905bcd094b4b11e445a94c86231eafe
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-07 12:13:46 +0100

    Refine training domain model

--- src/Smart.FA.Catalog.Core/Domain/Training/Training.cs
+++ src/Smart.FA.Catalog.Core/Domain/Training/Training.cs
@@ -1,3 +1,4 @@
+using Core.Domain.Enumerations;
 using Core.SeedWork;
 using Core.Services;
 
@@ -11,6 +12,7 @@ public class Training : Entity, IAggregateRoot
     private readonly List<TrainingIdentity> _Identities = new();
     private readonly List<TrainingTarget> _targets = new();
     private readonly List<TrainingDetail> _details = new();
+    private readonly List<TrainingSlot> _slots = new();
 
     #endregion
 
@@ -20,75 +22,119 @@ public class Training : Entity, IAggregateRoot
     public virtual IReadOnlyCollection<TrainingIdentity> Identities => _Identities;
     public virtual IReadOnlyCollection<TrainingTarget> Targets => _targets;
     public virtual IReadOnlyCollection<TrainingDetail> Details => _details;
+    public virtual IReadOnlyCollection<TrainingSlot> Slots => _slots;
 
-    public  int StatusId { get; private set; } = TrainingStatus.Draft.Id;
-
-    public int SlotNumberTypeId { get; private set; } = TrainingSlotNumberType.Group.Id;
-
+    public int TrainerCreatorId { get; }
+    public int StatusId { get; private set; } = TrainingStatus.Draft.Id;
 
     #endregion
 
     #region Constructors
 
-    public Training(Trainer trainer, IEnumerable<TrainingType> types, TrainingSlotNumberType slotNumberType,
+    public Training(Trainer trainer, IEnumerable<TrainingType> types,
+        IEnumerable<TrainingSlotNumberType> slotNumberTypes,
         IEnumerable<TrainingTargetAudience> targetAudiences)
     {
-        if (types == null || !types.Any()) throw new Exception();
-        if (targetAudiences == null || !targetAudiences.Distinct().Any()) throw new Exception();
-        if (trainer == null) throw new Exception();
-
         SwitchTrainingTypes(types);
         SwitchTargetAudience(targetAudiences);
-        SwitchSlotNumberType(slotNumberType);
+        SwitchSlotNumberType(slotNumberTypes);
         EnrollTrainer(trainer);
+        TrainerCreatorId = trainer.Id;
     }
 
     protected Training()
     {
-
     }
 
     #endregion
+
+    #region Public methods
+
     public void SwitchTrainingTypes(IEnumerable<TrainingType> trainingTypes)
     {
+        Guard.Requires(() => trainingTypes != null, "training types should not be null");
         _Identities.Clear();
         _Identities.AddRange(trainingTypes.Distinct().Select(trainingType => new TrainingIdentity(this, trainingType)));
     }
 
     public void SwitchTargetAudience(IEnumerable<TrainingTargetAudience> trainingTargetAudiences)
     {
+        Guard.Requires(() => trainingTargetAudiences != null, "training audiences should not be null");
         _targets.Clear();
         _targets.AddRange(trainingTargetAudiences.Distinct()
             .Select(trainingAudience => new TrainingTarget(this, trainingAudience)));
     }
 
-    public void SwitchSlotNumberType(TrainingSlotNumberType slotNumberType)
-        =>        SlotNumberTypeId = slotNumberType.Id;
+    public void SwitchSlotNumberType(IEnumerable<TrainingSlotNumberType> slotNumberTypes)
+    {
+        Guard.Requires(() => slotNumberTypes != null, "slot number types should not be null");
+        _slots.Clear();
+        _slots.AddRange(slotNumberTypes.Distinct()
+            .Select(slotNumberType => new TrainingSlot(this, slotNumberType)));
+    }
 
     public void EnrollTrainer(Trainer trainer)
     {
+        Guard.Requires(() => trainer != null, "There should be at least one trainer enrolled (owner)");
         TrainerEnrollment trainerEnrollment = new(this, trainer);
         if (_trainerEnrollments.Contains(trainerEnrollment)) throw new Exception();
         _trainerEnrollments.Add(trainerEnrollment);
     }
 
-    public void Validate(IMailService mailService)
+    public void EnrollTrainers(IEnumerable<Trainer> trainers)
     {
-        TrainingStatus status = Enumeration.FromValue<TrainingStatus>(StatusId);
+        DisenrollAll();
+        foreach (var trainer in trainers)
+        {
+            EnrollTrainer(trainer);
+        }
+    }
+
+    public void DisenrollAll()
+        => _trainerEnrollments.RemoveAll(enrollment => enrollment.TrainerId != TrainerCreatorId);
+
+    public bool Validate(IMailService mailService)
+    {
+        if (MissingFields().Any())
+        {
+            StatusId = TrainingStatus.Draft.Id;
+            return false;
+        }
+
+        var status = Enumeration.FromValue<TrainingStatus>(StatusId);
         StatusId = status.Validate(_Identities.Select(identity => identity.TrainingType)).Id;
+
+        return true;
     }
 
-    public void AddDetails(string title, string goal, string methodology, string language)
+    public void AddDetails(string title, string goal, string methodology, Language language)
     {
-        if (_details.FirstOrDefault(detail => detail.Language == language) != null) throw new Exception();
-        _details.Add(new TrainingDetail(this, title, goal, methodology, language));
+        Guard.Requires(() => _details.FirstOrDefault(detail => detail.Language == language.Value) == null,
+            "A description for that language already exists");
+        _details.Add(new TrainingDetail(this, title, goal, methodology, language.Value));
     }
 
-    public void UpdateDetails(string title, string goal, string methodology, string language)
+    public void UpdateDetails(string title, string goal, string methodology, Language language)
     {
-        var trainingDetail = _details.FirstOrDefault(detail => detail.Language == language);
-        if (trainingDetail == null) throw new Exception();
-        trainingDetail.UpdateDescription(title, goal, methodology);
+        Guard.Requires(() => _details.FirstOrDefault(detail => detail.Language == language.Value) != null,
+            "No descriptions for that language exist");
+       var detailToModify = _details.First(detail => detail.Language == language.Value);
+       detailToModify.Goal = goal;
+       detailToModify.Title = title;
+       detailToModify.Methodology = methodology;
+    }
+    #endregion
+
+    #region Private methods
+
+    private IEnumerable<string> MissingFields()
+    {
+        List<string> errors = new();
+        if (!Identities.Any()) errors.Add("Missing Identities");
+        if (!Targets.Any()) errors.Add("Missing Targets");
+        if (!TrainerEnrollments.Any()) errors.Add("Missing Trainer enrollments");
+        return errors;
     }
 
+    #endregion
 }
