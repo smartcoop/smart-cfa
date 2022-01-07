2eb1693e4dba91aa0aba10300ffe089b8358d76c
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-07 11:10:32 +0100

    Add cancelled in training status type enum

--- src/Smart.FA.Catalog.Core/Domain/Training/Enumerations/TrainingStatus.cs
+++ src/Smart.FA.Catalog.Core/Domain/Training/Enumerations/TrainingStatus.cs
@@ -7,6 +7,7 @@ public class TrainingStatus : Enumeration
     public static readonly TrainingStatus Draft = new(1, "Draft");
     public static readonly TrainingStatus WaitingForValidation = new(2, "WaitingForValidation");
     public static readonly TrainingStatus Validated = new(3, "Validated");
+    public static readonly TrainingStatus Cancelled = new(4, "Cancelled");
 
     private TrainingStatus(int id, string name) : base(id, name)
     {
