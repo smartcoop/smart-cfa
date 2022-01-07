cc075451655f60feaf094218c7596b82eaa712cc
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-07 11:44:49 +0100

    Add trainer repository implementation

--- src/Smart.FA.Catalog.Infrastructure/Persistence/Write/TrainerRepository.cs
+++ src/Smart.FA.Catalog.Infrastructure/Persistence/Write/TrainerRepository.cs
@@ -2,9 +2,9 @@ using Core.Domain;
 using Core.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Persistence.Repository;
+namespace Infrastructure.Persistence.Write;
 
-public class TrainerRepository: ITrainerRepository
+public class TrainerRepository : ITrainerRepository
 {
     private readonly Context _context;
 
@@ -12,13 +12,20 @@ public class TrainerRepository: ITrainerRepository
     {
         _context = context;
     }
-    public IEnumerable<Trainer> GetTrainersByTrainingId(int trainingId)
-    {
-        return  _context.TrainerEnrollments
-            .Where(enrollment => enrollment.TrainerId == trainingId)
-            .Include(enrollment => enrollment.Trainer)
-            .Select(enrollment => enrollment.Trainer);
-    }
 
-    public async Task<Trainer> FindByIdAsync(int trainerId) => await _context.Trainers.FirstOrDefaultAsync(trainer => trainer.Id == trainerId) ?? throw new InvalidOperationException();
+    public async Task<IEnumerable<Trainer>> GetListAsync(int trainingId, CancellationToken cancellationToken)
+        => await _context.TrainerEnrollments.Include(enrollment => enrollment.Trainer)
+            .Where(enrollment =>enrollment.TrainingId == trainingId)
+            .Select(enrollment => enrollment.Trainer)
+            .ToListAsync(cancellationToken);
+
+    public async Task<IEnumerable<Trainer>> GetListAsync(IEnumerable<int> trainingIds, CancellationToken cancellationToken)
+        => await _context.TrainerEnrollments.Include(enrollment => enrollment.Trainer)
+            .Where(enrollment => trainingIds.Contains(enrollment.TrainingId))
+            .Select(enrollment => enrollment.Trainer)
+            .ToListAsync(cancellationToken);
+
+    public async Task<Trainer> FindAsync(int trainerId, CancellationToken cancellationToken)
+        => await _context.Trainers.FirstOrDefaultAsync(trainer => trainer.Id == trainerId, cancellationToken) ??
+           throw new InvalidOperationException();
 }
