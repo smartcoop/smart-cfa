d8029abeac95e2de3046c0be3203b22db7e84295
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-20 10:44:52 +0100

    Make cancellation tokens optional in interfaces
    Default value of CancelationToken.None

--- src/Smart.FA.Catalog.Core/Domain/Trainer/Interfaces/ITrainerRepository.cs
+++ src/Smart.FA.Catalog.Core/Domain/Trainer/Interfaces/ITrainerRepository.cs
@@ -2,7 +2,7 @@ namespace Core.Domain.Interfaces;
 
 public interface ITrainerRepository
 {
-    Task<IReadOnlyList<Trainer>> GetListAsync(int trainingId, CancellationToken cancellationToken);
-    Task<IReadOnlyList<Trainer>> GetListAsync(IEnumerable<int> trainingIds, CancellationToken cancellationToken);
-    Task<Trainer> FindAsync(int trainerId, CancellationToken cancellationToken);
+    Task<IReadOnlyList<Trainer>> GetListAsync(int trainingId, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<Trainer>> GetListAsync(IEnumerable<int> trainingIds, CancellationToken cancellationToken = default);
+    Task<Trainer> FindAsync(int trainerId, CancellationToken cancellationToken = default);
 }
--- src/Smart.FA.Catalog.Core/Domain/Training/Interfaces/ITrainingRepository.cs
+++ src/Smart.FA.Catalog.Core/Domain/Training/Interfaces/ITrainingRepository.cs
@@ -4,7 +4,7 @@ namespace Core.Domain.Interfaces;
 
 public interface ITrainingRepository
 {
-    Task<IReadOnlyList<Training>> GetListAsync(int trainerId, Specification<Training> specification, CancellationToken cancellationToken);
-    Task<Training?> GetFullAsync(int trainingId, CancellationToken cancellationToken);
-    Task<Training> FindAsync(int trainingId, CancellationToken cancellationToken);
+    Task<IReadOnlyList<Training>> GetListAsync(int trainerId, Specification<Training> specification, CancellationToken cancellationToken = default);
+    Task<Training?> GetFullAsync(int trainingId, CancellationToken cancellationToken = default);
+    Task<Training> FindAsync(int trainingId, CancellationToken cancellationToken = default);
 }
