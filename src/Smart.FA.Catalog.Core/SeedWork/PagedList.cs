f9e9f5eb1aa69541a94131ed3798bde04e0b8c3d
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-01-20 11:05:03 +0100

    Comment choice for paging system
    A contrario of the default paginated list from microsoft documentation

--- src/Smart.FA.Catalog.Core/SeedWork/PagedList.cs
+++ src/Smart.FA.Catalog.Core/SeedWork/PagedList.cs
@@ -1,60 +1,37 @@
 namespace Core.SeedWork;
 
-public class PaginatedList<T> : List<T> {
-
-    public int PageIndex  { get; }
-    public int PageSize   { get; }
+/// <summary>
+/// The recommended Pagination class for EF-MVC of Microsoft (https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/sort-filter-page?view=aspnetcore-6.0)
+/// is incompatible with complex mapping in LINQ-TO-SQL scenario.
+/// For the specific use cases in the application, using the IQueryable interface is not possible for projecting tracked entities onto DTO models (used for displays only).
+/// The PagedList and the PagedObject (here-below) classes are the middle-ground that I found between performance and usability. PagedObject contains information for
+/// skipping and fetching a specific number of records according to the page while the PagedList keeps track of the total count of objects and pages. It uses IEnumerable
+/// instead of IQueriable (so dynamic LINQ-TO-SQL generation is not possible, which is actually good to maintain good decoupling of responsibilities)
+/// </summary>
+public class PagedList<T> : List<T>
+{
+    public int PageIndex { get; }
+    public int PageSize { get; }
     public int TotalCount { get; }
     public int TotalPages { get; }
 
-    public PaginatedList(IQueryable<T> source, int pageIndex, int pageSize) {
-        PageIndex = pageIndex;
-        PageSize = pageSize;
-        TotalCount = source.Count();
-        TotalPages = (int) Math.Ceiling(TotalCount / (double)PageSize);
-
-        this.AddRange(source.Skip(PageIndex * PageSize).Take(PageSize));
-    }
-
-    public bool HasPreviousPage {
-        get {
-            return (PageIndex > 0);
-        }
-    }
-
-    public bool HasNextPage {
-        get {
-            return (PageIndex+1 < TotalPages);
-        }
-    }
-}
-
-
-public class PagedList<T> : List<T> {
-
-    public int PageIndex  { get; }
-    public int PageSize   { get; }
-    public int TotalCount { get; }
-    public int TotalPages { get; }
-
-    public PagedList(IEnumerable<T> source, PageItem pageItem, int count) {
+    public PagedList(IEnumerable<T> source, PageItem pageItem, int count)
+    {
         PageIndex = pageItem.PageIndex;
         PageSize = pageItem.PageSize;
         TotalCount = count;
-        TotalPages = (int) Math.Ceiling(TotalCount / (double)PageSize);
+        TotalPages = (int) Math.Ceiling(TotalCount / (double) PageSize);
         this.AddRange(source);
     }
 
-    public bool HasPreviousPage {
-        get {
-            return (PageIndex > 0);
-        }
+    public bool HasPreviousPage
+    {
+        get { return (PageIndex > 0); }
     }
 
-    public bool HasNextPage {
-        get {
-            return (PageIndex+1 < TotalPages);
-        }
+    public bool HasNextPage
+    {
+        get { return (PageIndex + 1 < TotalPages); }
     }
 }
 
@@ -67,6 +44,7 @@ public class PageItem
     {
         get => CurrentPage - 1;
     }
+
     public int CurrentPage
     {
         get => _currentPage;
@@ -94,5 +72,4 @@ public class PageItem
         CurrentPage = currentPage;
         PageSize = pageSize;
     }
-
 }
