d7f10f7d834d386ae3a9d24906fd0adcb614087b
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-01 16:05:23 +0100

    Add migrations for trainer identity

---
+++ src/Smart.FA.Catalog.Infrastructure/Migrations/20220119153359_TrainerUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class TrainerUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ApplicationType",
+                table: "Trainer",
+                type: "int",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Trainer",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicationType",
+                table: "Trainer");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Trainer");
+        }
+    }
+}
