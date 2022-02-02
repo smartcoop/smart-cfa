db5b06e2266276c30446bb7071e7a440b804c5c7
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-02 10:00:58 +0100

    Set up static ip in a docker network

--- jenkins-run.sh
+++ jenkins-run.sh
@@ -1,15 +1,4 @@
 #! /usr/bin/env bash
 echo "run docker."
 
-docker run -d \
-  --name cfa-datasource \
-  --restart=always \
-  -p "1433:1433" \
-  cfa-datasource
-
-
-docker run -d \
-  --name cfa-api \
-  --restart=always \
-  -p "8080:80" \
-  cfa-api
+docker-compose up
