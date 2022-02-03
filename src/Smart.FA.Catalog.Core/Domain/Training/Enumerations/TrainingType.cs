db9f2460f5040967d0a8032f652f73549530fc24
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-03 12:19:45 +0100

    Refactor enumeration with private constructor

--- src/Smart.FA.Catalog.Core/Domain/Training/Enumerations/TrainingType.cs
+++ src/Smart.FA.Catalog.Core/Domain/Training/Enumerations/TrainingType.cs
@@ -8,9 +8,8 @@ public class TrainingType : Enumeration
     public static readonly TrainingType Professional = new(2, nameof(Professional));
     public static readonly TrainingType SchoolCourse = new(3, nameof(SchoolCourse));
     public static readonly TrainingType PermanentSchoolCourse = new(4, nameof(PermanentSchoolCourse));
-    // public static TrainingType Other = new(5, nameof(Other));
 
-    public TrainingType(int id, string name) : base(id, name)
+    private TrainingType(int id, string name) : base(id, name)
     {
     }
 
