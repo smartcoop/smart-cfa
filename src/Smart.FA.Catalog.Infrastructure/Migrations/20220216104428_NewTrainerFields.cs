a565369f0f30f72f7e923a3ac3e6e5af6f6df4a4
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-16 11:57:33 +0100

    Modify trainer fields to reflect figma CFA-157
    Add the Biography field

---
+++ src/Smart.FA.Catalog.Infrastructure/Migrations/20220216104428_NewTrainerFields.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class NewTrainerFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Trainer",
+                newName: "Biography");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TrainingDetail",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Trainer",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Trainer");
+
+            migrationBuilder.RenameColumn(
+                name: "Biography",
+                table: "Trainer",
+                newName: "Description");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TrainingDetail",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
