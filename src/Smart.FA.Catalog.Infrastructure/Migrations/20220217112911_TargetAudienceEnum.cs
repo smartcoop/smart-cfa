a5afa26603ffb92c9a28dbb2c0b5c05fd4610538
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-17 13:49:02 +0100

    Add training target audience mapping table to migrations

---
+++ src/Smart.FA.Catalog.Infrastructure/Migrations/20220217112911_TargetAudienceEnum.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class TargetAudienceEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrainingTargetAudience",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrainingTargetAudience", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TrainingTargetAudience",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Employee" });
+
+            migrationBuilder.InsertData(
+                table: "TrainingTargetAudience",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Student" });
+
+            migrationBuilder.InsertData(
+                table: "TrainingTargetAudience",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Unemployed" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrainingTargetAudience_Name",
+                table: "TrainingTargetAudience",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrainingTargetAudience");
+        }
+    }
+}
---
+++ src/Smart.FA.Catalog.Infrastructure/Persistence/Configuration/TrainingTargetAudienceConfigurations.cs
@@ -0,0 +1,9 @@
+using Core.Domain.Enumerations;
+using Infrastructure.Persistence.Configuration.EntityConfigurations;
+
+namespace Infrastructure.Persistence.Configuration;
+
+public class TrainingTargetAudienceConfigurations: EnumerationConfigurationBase<TrainingTargetAudience>
+{
+
+}
