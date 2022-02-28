abfb57819921403d86ad2774cd9ffa5b25c3d7bb
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-28 15:07:08 +0100

    Add EventIds for logging events + unit test for potential duplicate

---
+++ src/Smart.FA.Catalog.Core/LogEvents/TrainerLogEvents.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Core.LogEvents;
+
+public static partial class LogEventIds
+{
+    public static EventId TrainerCreated = new(20004, nameof(TrainerCreated));
+    public static EventId TrainerUpdated = new(20005, nameof(TrainerUpdated));
+    public static EventId TrainerDeleted = new(20006, nameof(TrainerDeleted));
+}
