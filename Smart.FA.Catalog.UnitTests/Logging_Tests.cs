a0a07a172778c619b7f496f147195a7c26cbf1d7
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-28 15:07:08 +0100

    Add EventIds for logging events + unit test for potential duplicate

---
+++ Smart.FA.Catalog.UnitTests/Logging_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using FluentAssertions;
+using LogEventIds = Core.LogEvents.LogEventIds;
+
+namespace Smart.FA.Catalog.UnitTests;
+
+public class LoggingTests
+{
+    public LoggingTests()
+    {
+
+    }
+    [Fact]
+    public static void CheckDuplicateLogEvents()
+    {
+        var eventType = typeof(LogEventIds);
+
+        var eventFields = eventType.GetFields().Select( fieldInfo => (EventId?) fieldInfo.GetValue(null) ?? 0);
+
+        eventFields.Should().OnlyHaveUniqueItems();
+    }
+}
