64769fa2ce35350653fe5fa3a410f73b9f23a0fc
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-28 15:07:08 +0100

    Add EventIds for logging events + unit test for potential duplicate

---
+++ src/Smart.FA.Catalog.Core/LogEvents/GeneralLogEvents.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Core.LogEvents;
+
+public static partial class LogEventIds
+{
+    public static EventId DuplicateEventId = new(00001, nameof(DuplicateEventId));
+}
---
+++ src/Smart.FA.Catalog.Core/LogEvents/TrainingLogEvents.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Core.LogEvents;
+
+public static partial class LogEventIds
+{
+    public static EventId TrainingCreated = new(10001, nameof(TrainingCreated));
+    public static EventId TrainingUpdated = new(10002, nameof(TrainingUpdated));
+    public static EventId TrainingDeleted = new(10003, nameof(TrainingDeleted));
+
+}
