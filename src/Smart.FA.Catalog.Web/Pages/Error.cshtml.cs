09a8e878289d4487a9bddc1d22699b2a1b689863
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-02-28 15:08:44 +0100

    Add Error page

---
+++ src/Smart.FA.Catalog.Web/Pages/Error.cshtml.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using Core.SeedWork;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel : PageModel
+{
+    private readonly ILogger<ErrorModel> _logger;
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    private string? ExceptionMessage { get; set; }
+
+    public ErrorModel(ILogger<ErrorModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+
+        var exceptionHandlerPathFeature =
+            HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+        if (exceptionHandlerPathFeature?.Error is DomainException domainException)
+        {
+            ExceptionMessage = GetStructuredErrorMessage(domainException.ToString(), RequestId, exceptionHandlerPathFeature.Path );
+        }
+        ExceptionMessage ??=
+            GetStructuredErrorMessage("unknown", RequestId,  exceptionHandlerPathFeature!.Path, exceptionHandlerPathFeature.Error!.Message);
+
+        _logger.LogError("{Message}", ExceptionMessage);
+    }
+
+    private string GetStructuredErrorMessage(string code, string requestId, string pagePath, string? message = null)
+        => $"request {requestId}-code {code}. On page {pagePath} {(message is null? string.Empty: ": " + message)}";
+}
