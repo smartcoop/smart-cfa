f036ab48b4e537b912b5e9d9cebe0b1b87198e2d
lammensmichel <lammens.michel@gmail.com>
2022-03-02 12:01:15 +0100

    Change docker build to docker-compose build for jenkins-build.sh

---
+++ jenkins-deploy.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+# This scripts is called by Jenkins whenever a branch is pushed to GitHub. See
+# the README.md file.
+
+set -e
+
+# Convert uppercase to lowercase.
+DOCKER_NAME=$(echo cfa_${BRANCH_NAME} | sed -e 's/\(.*\)/\L\1/' -e 's/origin\/master/master/g' -e 's/origin\/feature\//feature-/g' -e 's/origin\/hotfix\//hotfix-/g' -e 's/feature\//feature-/g' -e 's/hotfix\//hotfix-/g')
+DOCKER_HOSTNAME=$(echo ${BRANCH_NAME} | sed -e 's/\(.*\)/\L\1/')
+
+echo
+if ! echo ${DOCKER_NAME} |  grep -q "^[a-z][a-z0-9_-]*$"; then
+  echo "The branch does not use only alphanumeric characters and dashes. Exiting.";
+  exit 1
+fi
+
+# TODO HTML-escape those strings, especially the subject.
+GIT_COMMIT="$(git rev-parse HEAD)"
+GIT_COMMIT_SUBJECT="$(git log -n1 --format=format:"%s")"
+GIT_COMMIT_DATE="$(git log -n1 --format=format:"%ci")"
+GIT_AUTHOR_NAME="$(git log -n1 --format=format:"%aN")"
+GIT_AUTHOR_EMAIL="$(git log -n1 --format=format:"%cE")"
+
+echo "Starting jenkins-deploy.sh script...."
+echo "Branch name: ${DOCKER_HOSTNAME}"
+echo "Domain: ${DOCKER_HOSTNAME}.smartcoop.dev"
+echo "Git commit: ${GIT_COMMIT}"
+echo "Git subject: ${GIT_COMMIT_SUBJECT}"
+echo "Git date: ${GIT_COMMIT_DATE}"
+echo "Git author name: ${GIT_AUTHOR_NAME}"
+echo "Git author email: ${GIT_AUTHOR_EMAIL}"
+
+echo "DOCKER_NAME=${DOCKER_NAME}" > .env
+
+sed -e "s/host.docker.internal/smart-${DOCKER_NAME}_datasource_1/" \
+   ./src/Smart.FA.Catalog.Web/appsettings.Development.json > ./src/Smart.FA.Catalog.Web/appsettings.DevelopmentTmp.json
+mv ./src/Smart.FA.Catalog.Web/appsettings.DevelopmentTmp.json ./src/Smart.FA.Catalog.Web/appsettings.Development.json
+
+echo "build docker DB and API"
+docker-compose --env-file ./.env build
+echo "stop old docker"
+docker-compose down || true
+echo "run new docker."
+docker-compose --env-file ./.env up -d
