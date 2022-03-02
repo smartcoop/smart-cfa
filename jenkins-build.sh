3ab08b6214c793e3358b7e486b5b71ca3b046fe3
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-03-02 13:00:15 +0100

    Change docker instructions to docker-compose for build in jenkins-build.sh

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -2,23 +2,4 @@
 
 echo "Clean images."
 
-docker rm -f "cfa-datasource" || true
-docker rm -f "cfa-api" || true
-
-docker image rm "cfa-api" || true
-docker image rm "cfa-datasource" || true
-
-docker image prune -f
-
-echo "build docker."
-
-docker build  \
-  -f Api.Dockerfile \
-  -t cfa-api \
-  .
-
-
-docker build  \
-  -f DB.Dockerfile \
-  -t cfa-datasource \
-  .
+docker-compose build
