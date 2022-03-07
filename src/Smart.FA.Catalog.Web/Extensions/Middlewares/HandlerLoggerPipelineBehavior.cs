e3b27d92d3dbea88861dcb175a42e6c422c5ca87
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-03-07 11:26:58 +0100

    Add behavior pipeline for logging in handlers
    Generic logging behavior for query and command handlers

---
+++ src/Smart.FA.Catalog.Web/Extensions/Middlewares/HandlerLoggerPipelineBehavior.cs
@@ -0,0 +1,54 @@
+﻿using Application.SeedWork;
+using Core.Exceptions;
+using Core.LogEvents;
+using Core.SeedWork;
+using MediatR;
+
+namespace Web.Extensions.Middlewares;
+
+/// <summary>
+/// Global logger applied on all handlers.
+/// It will format potential exception with the Error template when pertinent and set the response to a successful state, otherwise.
+/// </summary>
+/// <typeparam name="TRequest">The input object of the handler, it needs to inherit from IMediatr IRequest class</typeparam>
+/// <typeparam name="TResponse">the output object of the handler, it needs to inherit from ResponseBase and be instantiable</typeparam>
+public class HandlerLoggerPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse> where TResponse : ResponseBase, new()
+{
+    private readonly ILogger<HandlerLoggerPipelineBehavior<TRequest, TResponse>> _logger;
+
+    public HandlerLoggerPipelineBehavior
+    (
+        ILogger<HandlerLoggerPipelineBehavior<TRequest, TResponse>> logger
+    )
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle
+    (
+        TRequest request
+        , CancellationToken cancellationToken,
+        RequestHandlerDelegate<TResponse> next
+    )
+    {
+        TResponse response = new();
+        try
+        {
+            response = await next();
+            response.SetSuccess();
+        }
+        catch (DomainException exception)
+        {
+            response.AddError(exception.Error);
+            _logger.LogError(LogEventIds.ErrorEventId, exception.Error.Serialize());
+        }
+        catch (Exception exception)
+        {
+            response.AddError(Errors.General.UnexpectedHandlerError());
+            _logger.LogError(LogEventIds.ErrorEventId, exception.Message);
+        }
+
+        return response;
+    }
+}
--- src/Smart.FA.Catalog.Web/Extensions/ServiceCollectionExtensions.cs
+++ src/Smart.FA.Catalog.Web/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 using Core.Services;
+using MediatR;
+using Web.Extensions.Middlewares;
 using Web.Identity;
 using Web.Options;
 
@@ -8,12 +10,21 @@ public static class ServiceCollectionExtensions
 {
     public static void AddApi(this IServiceCollection services, IConfigurationSection adminOptionsSection)
     {
+
         services.AddScoped<IUserIdentity, UserIdentity>();
         services.AddOptions(adminOptionsSection);
+        services.AddCustomMediatR();
     }
 
-    public static void AddOptions(this IServiceCollection services, IConfigurationSection adminOptionsSection)
+    public static IServiceCollection AddOptions(this IServiceCollection services, IConfigurationSection adminOptionsSection)
     {
         services.Configure<AdminOptions>(adminOptionsSection);
+        return services;
+    }
+
+    public static IServiceCollection AddCustomMediatR(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(HandlerLoggerPipelineBehavior<,>));
+        return services;
     }
 }
