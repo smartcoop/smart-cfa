7293a6d2820e52f094ee70927233a163baa7d8f6
Maxime Poulain <maxime.poulain@smart.coop>
2022-03-08 19:42:39 +0100

    Refactor the javascript that scroll and focus to the first HTML element in an error state
    this commit refactors the code that scroll and focus the first HTML element that is in an error state.

--- src/Smart.FA.Catalog.Web/wwwroot/js/lib.js
+++ src/Smart.FA.Catalog.Web/wwwroot/js/lib.js
@@ -8,3 +8,22 @@ $(document).ready(
             model.value = true;
         });
     });
+
+// Scrolls to the first HTML element that has a class attribute ending with '--error'
+function scrollToFirstErrorElement() {
+    let elementsInError = document.querySelectorAll("*[class*='--error']");
+    if (elementsInError.length) {
+        let firstInvalidControl = elementsInError[0];
+        let labelOffset = 500;
+        window.scroll({
+            top: (firstInvalidControl.getBoundingClientRect().top + window.scrollY - labelOffset),
+            left: 0,
+            behavior: "smooth"
+        });
+        firstInvalidControl.focus();
+    };
+}
+
+function RegisterScrollToFirstElementInError() {
+    window.addEventListener("load", scrollToFirstErrorElement);
+}
