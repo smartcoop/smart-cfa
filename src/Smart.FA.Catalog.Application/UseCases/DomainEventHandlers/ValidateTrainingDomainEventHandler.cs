419893c5b3c3cbfabeebb7179dd226d85d371399
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-03-21 13:23:14 +0100

    Refactor event dispatcher
    dispatching domain event incumbents now to the mediator (it publishes events picked up that are by specific domain event handlers)

---
+++ src/Smart.FA.Catalog.Application/UseCases/DomainEventHandlers/ValidateTrainingDomainEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Core.Domain;
+using Core.SeedWork;
+using MediatR;
+
+namespace Application.UseCases.DomainEventHandlers;
+
+public class ValidateTrainingDomainEventHandler : INotificationHandler<ValidateTrainingEvent>
+{
+    private readonly MessageBus _messageBus;
+
+    public ValidateTrainingDomainEventHandler(MessageBus messageBus)
+    {
+        _messageBus = messageBus;
+    }
+
+    public Task Handle(ValidateTrainingEvent notification, CancellationToken cancellationToken)
+    {
+        _messageBus.ValidateTrainingMessage(notification.TrainingId,
+            notification.TrainingName, notification.TrainersId);
+
+        return Task.CompletedTask;
+    }
+}
--- src/Smart.FA.Catalog.Core/SeedWork/EventDispatcher.cs
+++ src/Smart.FA.Catalog.Core/SeedWork/EventDispatcher.cs
@@ -1,34 +1,26 @@
 using Core.Domain;
+using MediatR;
 
 namespace Core.SeedWork;
 
 public class EventDispatcher
 {
-    private readonly MessageBus _messageBus;
-
-    public EventDispatcher(MessageBus messageBus)
+    private readonly IMediator _mediator;
+    public EventDispatcher(IMediator mediator)
     {
-        _messageBus = messageBus;
+        _mediator = mediator;
     }
 
-    public void Dispatch(IEnumerable<DomainEvent> domainEvents)
+    public async Task Dispatch(IEnumerable<DomainEvent> domainEvents)
     {
         foreach (var domainEvent in domainEvents)
         {
-            Dispatch(domainEvent);
+           await Dispatch(domainEvent);
         }
     }
 
-    public void Dispatch(DomainEvent domainEvent)
+    private async Task Dispatch(DomainEvent domainEvent)
     {
-        switch (domainEvent)
-        {
-            case ValidateTrainingEvent validation:
-                _messageBus.ValidateTrainingMessage(validation.TrainingId,
-                    validation.TrainingName, validation.TrainersId);
-                break;
-            default:
-                throw new Exception($"Unknown event type: '{domainEvent.GetType()}'");
-        }
+        await _mediator.Publish(domainEvent);
     }
 }
