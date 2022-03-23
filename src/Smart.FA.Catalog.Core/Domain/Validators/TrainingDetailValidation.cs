8d39dace7f2ecb6ff84d04c63634b7a7b98e3f07
Maxime Poulain <90202188+maxime-poulain@users.noreply.github.com>
2022-03-24 00:37:07 +0100

    Refactor training creation and deletion validators (#44)
    Improves a bit the readability of those validators.

--- src/Smart.FA.Catalog.Core/Domain/Validators/TrainingDetailValidation.cs
+++ src/Smart.FA.Catalog.Core/Domain/Validators/TrainingDetailValidation.cs
@@ -8,12 +8,12 @@ public class TrainingDetailValidation: AbstractValidator<TrainingDetail>
     public TrainingDetailValidation()
     {
         RuleFor(request => request.Title)
-            .NotEmpty().WithMessage(Errors.General.MissingField("title").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("title").Message);
         RuleFor(request => request.Language)
-            .NotEmpty().WithMessage(Errors.General.MissingField("language").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("language").Message);
         RuleFor(request => request.Goal)
-            .NotEmpty().WithMessage(Errors.General.MissingField("goal").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("goal").Message);
         RuleFor(request => request.Methodology)
-            .NotEmpty().WithMessage(Errors.General.MissingField("methodology").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("methodology").Message);
     }
 }
--- src/Smart.FA.Catalog.Core/Domain/Validators/TrainingValidator.cs
+++ src/Smart.FA.Catalog.Core/Domain/Validators/TrainingValidator.cs
@@ -8,17 +8,17 @@ public class TrainingValidator: AbstractValidator<Training>
     public TrainingValidator()
     {
         RuleFor(request => request.Identities)
-            .NotEmpty().WithMessage(Errors.General.MissingField("identity").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("identity").Message);
         RuleFor(request => request.Targets)
-            .NotEmpty().WithMessage(Errors.General.MissingField("target").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("target").Message);
         RuleFor(request => request.Topics)
-            .NotEmpty().WithMessage(Errors.General.MissingField("topic").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("topic").Message);
         RuleFor(request => request.TrainerAssignments)
-            .NotEmpty().WithMessage(Errors.General.MissingField("trainer").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("trainer").Message);
         RuleFor(request => request.TrainerAssignments)
-            .NotEmpty().WithMessage(Errors.General.MissingField("trainer").Message);
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("trainer").Message);
         RuleFor(request => request.Details)
-            .NotEmpty().WithMessage(Errors.General.MissingField("description").Message)
+            .NotEmptyWithGenericMessage().WithMessage(Errors.General.MissingField("description").Message)
             .ForEach(detail => detail.SetValidator(new TrainingDetailValidation()));
     }
 }
