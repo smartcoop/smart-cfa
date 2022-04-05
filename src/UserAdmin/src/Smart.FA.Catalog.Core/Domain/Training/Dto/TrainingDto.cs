fe780479831dd7d22dda1d5b77e0cbfdd82eaa28
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-04-05 14:51:23 +0200

    Map topic id sto a list in the training dto to fix broken paging
    Make use of the multi-mapping feature of Dapper to map to a list

--- src/UserAdmin/src/Smart.FA.Catalog.Core/Domain/Training/Dto/TrainingDto.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Core/Domain/Training/Dto/TrainingDto.cs
@@ -1,9 +1,41 @@
 namespace Smart.FA.Catalog.Core.Domain.Dto;
 
-public record TrainingDto
-    (int TrainingId,
-    short StatusId,
-    string Title,
-    string? Goal,
-    string Language,
-    int TopicId);
+//Because of some restrictions with Dapper*, we cannot use a record here
+//*Dapper split-on feature seems to only support a mutable list of topic ids in its constructor
+public class TrainingDto
+{
+    public int TrainingId { get; }
+    public short StatusId { get; }
+    public string Title { get; }
+    public string? Goal { get; }
+    public string Language { get; }
+    public List<int>? TopicIds { get; }
+
+    public TrainingDto(int trainingId,
+        short statusId,
+        string title,
+        string? goal,
+        string language,
+        List<int> topicIds)
+    {
+        TrainingId = trainingId;
+        StatusId = statusId;
+        Title = title;
+        Goal = goal;
+        Language = language;
+        TopicIds = topicIds;
+    }
+    public TrainingDto(int trainingId,
+        short statusId,
+        string title,
+        string? goal,
+        string Language)
+    {
+        this.TrainingId = trainingId;
+        this.StatusId = statusId;
+        this.Title = title;
+        this.Goal = goal;
+        this.Language = Language;
+        this.TopicIds = new List<int>();
+    }
+}
--- src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Persistence/Read/TrainingQueries.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Persistence/Read/TrainingQueries.cs
@@ -51,9 +51,19 @@ public class TrainingQueries : ITrainingQueries
                     INNER JOIN dbo.TrainingDetail TD ON T.Id = TD.TrainingId
                     WHERE TE.TrainerId = @TrainerId AND TD.Language = @Language ";
         await using var connection = new SqlConnection(_connectionString);
-        return await connection.QueryAsync<TrainingDto>(sql, new {trainerId, language});
+        return await connection.QueryAsync<TrainingDto, int?, TrainingDto>(sql, (dto, topicId) =>
+            {
+                if (topicId is not null)
+                {
+                    dto.TopicIds?.Add((int) topicId);
+                }
+                return dto;
+            },
+            splitOn: "TopicId",
+            param: new {trainerId, language});
     }
 
+    //TODO: it's a bit heavy for a simple paging system (though it works), we might want to look to refactor that at some point
     public async Task<PagedList<TrainingDto>> GetPagedListAsync(int trainerId, string language, PageItem pageItem,
         CancellationToken cancellationToken)
     {
@@ -64,29 +74,50 @@ public class TrainingQueries : ITrainingQueries
 	                    TD.Goal,
                         TD.Language,
                         TC.TrainingTopicId 'TopicId'
-                    FROM dbo.Training T
-                    INNER JOIN dbo.TrainerAssignment TE ON T.Id = TE.TrainingId
-                    LEFT JOIN dbo.TrainingCategory TC ON T.Id = TC.TrainingId
-                    INNER JOIN dbo.TrainingDetail TD ON T.Id = TD.TrainingId
-                    WHERE TE.TrainerId = @TrainerId AND TD.Language = @Language
-                    ORDER BY T.Id
-                    OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY";
-
-        var countsql = @"SELECT
-	                    COUNT(*)
-                    FROM dbo.Training T
+                    FROM (SELECT * FROM dbo.Training T ORDER BY T.Id
+                    OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY) T
                     INNER JOIN dbo.TrainerAssignment TE ON T.Id = TE.TrainingId
                     LEFT JOIN dbo.TrainingCategory TC ON T.Id = TC.TrainingId
                     INNER JOIN dbo.TrainingDetail TD ON T.Id = TD.TrainingId
                     WHERE TE.TrainerId = @TrainerId AND TD.Language = @Language";
 
+        var countsql = @"SELECT
+	                        COUNT(*)
+                        FROM (SELECT T.Id FROM dbo.Training T
+                        INNER JOIN dbo.TrainerAssignment TE ON T.Id = TE.TrainingId
+                        LEFT JOIN dbo.TrainingCategory TC ON T.Id = TC.TrainingId
+                        INNER JOIN dbo.TrainingDetail TD ON T.Id = TD.TrainingId
+                        WHERE TE.TrainerId = @TrainerId AND TD.Language = @Language
+                        GROUP BY T.Id) A";
+
         await using var connection = new SqlConnection(_connectionString);
         await connection.OpenAsync(cancellationToken);
         await using var transaction = connection.BeginTransaction();
+
+        //Fetch the count of distinct trainings
         var count = await transaction.QuerySingleAsync<int>(countsql, new {trainerId, language});
-        var list = await transaction.QueryAsync<TrainingDto>(sql,
-            new {trainerId, language, pageItem.Offset, pageItem.PageSize});
+        //Get a list of training and splits field on the training topic id
+        var list = await transaction.QueryAsync<TrainingDto, int?, TrainingDto>(sql, (dto, topicId) =>
+            {
+                if (topicId is not null)
+                {
+                    dto.TopicIds?.Add((int) topicId);
+                }
+
+                return dto;
+            },
+            splitOn: "TopicId",
+            param: new {trainerId, language, pageItem.Offset, pageItem.PageSize});
         transaction.Commit();
-        return new PagedList<TrainingDto>(list, pageItem, count);
+        //Regroup all training records by id
+        var result = list.GroupBy(dto => dto.TrainingId).Select(trainingGroup =>
+            new TrainingDto(trainingGroup.Key,
+                trainingGroup.First().StatusId,
+                trainingGroup.First().Title,
+                trainingGroup.First().Goal,
+                trainingGroup.First().Language,
+                trainingGroup.SelectMany(tg => tg.TopicIds).ToList()));
+
+        return new PagedList<TrainingDto>(result, pageItem, count);
     }
 }
