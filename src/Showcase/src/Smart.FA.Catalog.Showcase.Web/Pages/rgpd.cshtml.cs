51744f9a26ab452864b31cff3c735e346ae32b8e
Joelle Boucher <joe@smart.coop>
2022-04-08 16:29:42 +0200

    add cshtml and cshtml.cs structure page

---
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Pages/rgpd.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.FA.Catalog.Showcase.Web.Pages
+{
+    public class rgpdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
