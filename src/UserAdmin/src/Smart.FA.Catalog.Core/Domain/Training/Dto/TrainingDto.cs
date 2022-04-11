d3391b6f59bdb0fc9998a5e4889eb629ea9ec0a8
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-04-11 10:54:34 +0200

    Add space between comment line training dto

--- src/UserAdmin/src/Smart.FA.Catalog.Core/Domain/Training/Dto/TrainingDto.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Core/Domain/Training/Dto/TrainingDto.cs
@@ -1,7 +1,7 @@
 namespace Smart.FA.Catalog.Core.Domain.Dto;
 
-//Because of some restrictions with Dapper*, we cannot use a record here
-//*Dapper split-on feature seems to only support a mutable list of topic ids in its constructor
+// Because of some restrictions with Dapper*, we cannot use a record here
+// *Dapper split-on feature seems to only support a mutable list of topic ids in its constructor
 public class TrainingDto
 {
     public int TrainingId { get; }
