584bcf8913bc592e8bde7445251eb8fd749a8268
Maxime Poulain <maxime.poulain@smart.coop>
2022-04-14 20:39:25 +0200

    TrainerConfiguration: add constraint length on email and language
    This commit makes maximum length on email and default language respectively 254 and 2 characters long.

--- src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220414183714_Add_Fixed_Length_On_Trainer_Language_And_Email.Designer.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220414183714_Add_Fixed_Length_On_Trainer_Language_And_Email.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Smart.FA.Catalog.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using Smart.FA.Catalog.Infrastructure.Persistence;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogContext))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220414183714_Add_Fixed_Length_On_Trainer_Language_And_Email")]
+    partial class Add_Fixed_Length_On_Trainer_Language_And_Email
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
