68f9048b0339efb19d829f50014831d20e539caf
Joe <joe@ubik.be>
2022-04-14 12:12:08 +0200

    Introduce the create rgpd public page for the showcase website (#68)
    This commit introduces the RGPD page with HTML structure.
    This commit also fixes the solution configuration file (.sln). 
    
    Co-authored-by: Joelle Boucher <joe@smart.coop>
    Co-authored-by: Joëlle <41791847+joe-coop@users.noreply.github.com>

---
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Pages/Rgpd.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart.FA.Catalog.Showcase.Web.Pages;
+
+public class RgpdModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
