c67aa3342e145577b0c84e1188d41b69f03d9d51
Maxime Poulain <maxime.poulain@smart.coop>
2022-04-14 19:28:34 +0200

    Introduce localization for the showcase project

---
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Extensions/LocalizationRegistration.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
+namespace Smart.FA.Catalog.Showcase.Web.Extensions;
+
+public static class LocalizationRegistration
+{
+    public static IServiceCollection AddShowcaseLocalization(this IServiceCollection services)
+    {
+        return services
+            .AddLocalization()
+            .Configure<RequestLocalizationOptions>(
+                options =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                    {
+                        new("en"),
+                        new("nl"),
+                        new("fr")
+                    };
+
+                    // Requirements are to have french has default language.
+                    options.DefaultRequestCulture = new RequestCulture("fr", "fr");
+
+                    // Formatting numbers, dates, etc.
+                    options.SupportedCultures = supportedCultures;
+
+                    // UI strings that we have localized.
+                    options.SupportedUICultures = supportedCultures;
+                }
+            );
+    }
+}
--- src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Program.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Program.cs
@@ -1,7 +1,12 @@
+using Smart.FA.Catalog.Showcase.Web.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add localization.
+builder.Services.AddShowcaseLocalization();
 
 var app = builder.Build();
 
