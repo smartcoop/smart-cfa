b93c0375444d5372a2be54e138000e0467bfb4da
Victor van Duijnen <victor.van.duijnen@smart.coop>
2022-04-15 17:12:31 +0200

    Use IUserIdenty + refactor linq statetement
    Linq statement correspond now to 1 sql query

--- src/UserAdmin/src/Smart.FA.Catalog.Web/Policies/Requirements/AtLeastOneValidUserChartApprovalRequirement.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Web/Policies/Requirements/AtLeastOneValidUserChartApprovalRequirement.cs
@@ -3,7 +3,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Smart.FA.Catalog.Core.Domain.Models;
+using Smart.FA.Catalog.Core.Services;
 using Smart.FA.Catalog.Infrastructure.Persistence;
 
 namespace Smart.FA.Catalog.Web.Policies.Requirements;
@@ -11,34 +13,38 @@ namespace Smart.FA.Catalog.Web.Policies.Requirements;
 /// <summary>
 /// Require that the user have already accepted at least one valid user chart
 /// </summary>
-public class AtLeastOneValidUserChartApprovalRequirement : IAuthorizationRequirement { }
+public class AtLeastOneValidUserChartApprovalRequirement : IAuthorizationRequirement
+{
+}
 
 public class UserChartApprovalHandler : AuthorizationHandler<AtLeastOneValidUserChartApprovalRequirement>
 {
     private readonly CatalogContext _catalogContext;
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly IUserIdentity _userIdentity;
 
-    public UserChartApprovalHandler(CatalogContext catalogContext, IHttpContextAccessor httpContextAccessor)
+    public UserChartApprovalHandler(CatalogContext catalogContext, IHttpContextAccessor httpContextAccessor, IUserIdentity userIdentity)
     {
         _catalogContext = catalogContext;
         _httpContextAccessor = httpContextAccessor;
+        _userIdentity = userIdentity;
     }
 
-    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, AtLeastOneValidUserChartApprovalRequirement requirement)
+    protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, AtLeastOneValidUserChartApprovalRequirement requirement)
     {
         var currentDate = DateTime.UtcNow;
-        var identity = (CustomIdentity) context.User.Identity!;
-        var validUserChartsApprovals = _catalogContext.Trainers
-            .FirstOrDefault(trainer => trainer.Id == identity.Id)?
-            .Approvals
-            .Where(trainerApproval => (trainerApproval.UserChart.ValidityDate == null || trainerApproval.UserChart.ValidityDate!.Value.Ticks >= currentDate.Ticks) &&
-                                      (trainerApproval.UserChart.ExpirationDate == null || trainerApproval.UserChart.ExpirationDate!.Value.Ticks <= currentDate.Ticks));
-        if (validUserChartsApprovals is not null && validUserChartsApprovals.Any())
+
+        var hasTrainerValidUserChartApprovals = await _catalogContext.Trainers
+            .Where(trainer => trainer.Id == _userIdentity.Id)
+            .Where(trainer => trainer.Approvals.Any(approval =>
+                currentDate >= approval.UserChartRevision.ValidFrom.Date &&
+                (approval.UserChartRevision.ValidUntil == null || currentDate <= approval.UserChartRevision.ValidUntil!.Value.Date)))
+            .AnyAsync();
+
+        if (hasTrainerValidUserChartApprovals)
         {
-            _httpContextAccessor.HttpContext!.Response.Cookies.Append("HasAcceptedUserChart", "true", new CookieOptions{MaxAge = TimeSpan.FromMinutes(1)});
+            _httpContextAccessor.HttpContext!.Response.Cookies.Append("HasAcceptedUserChart", "true", new CookieOptions {MaxAge = TimeSpan.FromMinutes(1)});
             context.Succeed(requirement);
         }
-
-        return Task.CompletedTask;
     }
 }
