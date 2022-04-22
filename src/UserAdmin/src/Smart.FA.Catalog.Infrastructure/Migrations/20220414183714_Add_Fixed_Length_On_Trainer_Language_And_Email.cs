32f9bccfffb86a5a6264392ead5e23f053f840a8
Maxime Poulain <90202188+maxime-poulain@users.noreply.github.com>
2022-04-22 11:33:15 +0200

    TrainerConfiguration: add constraint length on email and language (#76)
    This commit makes maximum length on email and default language respectively 254 and 2 characters long.

---
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220414183714_Add_Fixed_Length_On_Trainer_Language_And_Email.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Add_Fixed_Length_On_Trainer_Language_And_Email : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Trainer",
+                type: "nvarchar(254)",
+                maxLength: 254,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DefaultLanguage",
+                table: "Trainer",
+                type: "nchar(2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Trainer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(254)",
+                oldMaxLength: 254,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DefaultLanguage",
+                table: "Trainer",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nchar(2)");
+        }
+    }
+}
