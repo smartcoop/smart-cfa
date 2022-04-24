d983668714cc5db01f62e82a55a891962bfbefb8
joe-ubik <joe@ubik.be>
2022-04-24 15:58:13 +0200

    Some new keys for localization

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Localization/ShowcaseResources.Designer.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Localization/ShowcaseResources.Designer.cs
@@ -72,6 +72,24 @@ namespace Smart.FA.Catalog.Showcase.Localization {
         /// <summary>
         ///   Looks up a localized string similar to Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in ullamcorper tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis eget varius nisl, et leifend est. In condimentum, arcu eget ornare aliquam, urna quam luctus quam, ut vestibulum nulla turpis ultricies lectus. Nunc gravida gravida feugiat. Quisque pellentesque libero in massa fringilla, vel gravida ex ultricies. Proin in mi dolor. Fusce mollis neque tellus, vel malesuada nibh malesuada vitae. Cur [rest of string was truncated]&quot;;.
         /// </summary>
+        public static string Contact_Presentation {
+            get {
+                return ResourceManager.GetString("Contact_Presentation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Contact Formateurs Associés.
+        /// </summary>
+        public static string Contact_TitlePage {
+            get {
+                return ResourceManager.GetString("Contact_TitlePage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in ullamcorper tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis eget varius nisl, et leifend est. In condimentum, arcu eget ornare aliquam, urna quam luctus quam, ut vestibulum nulla turpis ultricies lectus. Nunc gravida gravida feugiat. Quisque pellentesque libero in massa fringilla, vel gravida ex ultricies. Proin in mi dolor. Fusce mollis neque tellus, vel malesuada nibh malesuada vitae. Cur [rest of string was truncated]&quot;;.
+        /// </summary>
         public static string LoremIpsum {
             get {
                 return ResourceManager.GetString("LoremIpsum", resourceCulture);
