6de10b1e29096327df706fc55133d5878394d208
Nour Jammal <naj@smart.coop>
2022-04-26 09:41:27 +0200

    Better readability in SQL

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Infrastructure/Data/Scripts/v_TrainingDetails.sql
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Infrastructure/Data/Scripts/v_TrainingDetails.sql
@@ -1,7 +1,16 @@
 CREATE VIEW [dbo].[v_TrainingDetails]
 AS
-SELECT training.Id, detail.Title, detail.Methodology, detail.Goal, detail.PracticalModalities, category.TrainingTopicId, detail.Language, trainer.FirstName,
-trainer.LastName,trainer.Title AS trainerTitle, training.StatusId
+SELECT training.Id
+        ,detail.Title
+        ,detail.Methodology
+        ,detail.Goal
+        ,detail.PracticalModalities
+        ,category.TrainingTopicId
+        ,detail.Language
+        ,trainer.FirstName
+        ,trainer.LastName
+        ,trainer.Title AS trainerTitle
+        ,training.StatusId
 FROM [Catalog].[dbo].[Training] AS training
 JOIN [Catalog].[dbo].[TrainingDetail] AS detail ON training.Id = detail.TrainingId
 JOIN [Catalog].[dbo].[TrainerAssignment] trainingTrainer ON training.Id = trainingTrainer.TrainingId
