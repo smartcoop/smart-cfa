4522a4224cf518a04a243b793e394afd9e6b2241
CelineMarique <41579916+CelineMarique@users.noreply.github.com>
2022-04-26 11:30:26 +0200

    Feature/cfa 313 add training details page (#75)
    CFA - Showcase > Add training details page
    https://smartjira.atlassian.net/browse/CFA-313
    
    * Add SQL view for training detail
    
    And add script into Infrastructure project.
    
    * Add db context and model for training detail
    
    Add model into Domain project.
    Add context into Infractructure project.
    Adapt DbContext to add keys.
    
    * Add training details page
    
    Add folder and view model.
    Adapt links.
    Map to view model
    
    * Add  Smart Design 404 page
    
    * Add Routes class
    
    * ﻿Add trainer title & Training practical modalities
    
    in trainingDetails view
    
    * Improve readability for sql views
    
    Co-authored-by: NourJammal <naj@smart.coop>
    Co-authored-by: CelineMarique <mar@smart.coop>
    Co-authored-by: ChMelissa <chm@smart.coop>

---
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Infrastructure/Data/Scripts/v_TrainingDetails.sql
@@ -0,0 +1,18 @@
+CREATE VIEW [dbo].[v_TrainingDetails]
+AS
+SELECT training.Id
+        ,detail.Title
+        ,detail.Methodology
+        ,detail.Goal
+        ,detail.PracticalModalities
+        ,category.TrainingTopicId
+        ,detail.Language
+        ,trainer.FirstName
+        ,trainer.LastName
+        ,trainer.Title AS trainerTitle
+        ,training.StatusId
+FROM [Catalog].[dbo].[Training] AS training
+JOIN [Catalog].[dbo].[TrainingDetail] AS detail ON training.Id = detail.TrainingId
+JOIN [Catalog].[dbo].[TrainerAssignment] trainingTrainer ON training.Id = trainingTrainer.TrainingId
+JOIN [Catalog].[dbo].[Trainer] trainer ON trainingTrainer.TrainerId = trainer.Id
+JOIN [Catalog].[dbo].[TrainingCategory] category ON training.Id = category.TrainingId
