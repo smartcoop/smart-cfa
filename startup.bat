b996295c843e12028c37ce20d27bb4f261cb11b2
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-04-27 15:02:34 +0200

    Remove linux fix (docker compose is a windows alias)

--- startup.bat
+++ startup.bat
@@ -1,33 +1,12 @@
-:: Using a little trick (goto instruction will be interpreted as a command which goes to label '(){' in windows but as a function in linux)
-:: we can make a script compatible for both linux and windows os, we just need to give execution rights to the file
-:: https://nastytester.com/posts/script-that-works-in-windows-and-linux.html
-goto(){
-    :: Stop potentially running services
-    echo "Stopping docker containers..."
-    docker compose -f ./docker-compose-startup.yml stop
-
-    :: Down and remove images
-    echo "Removing docker containers and images"
-    docker compose -f ./docker-compose-startup.yml down --rmi all
-
-    :: launch specific docker compose (local) and remove orphans (previous containers that could have been removed from a docker compose)
-    echo "Re-creating docker images and running containers"
-    docker compose -f ./docker-compose-startup.yml up --remove-orphans
-}
-
-goto $@
-exit
-
-:(){
 :: Stop potentially running services
 echo "Stopping docker containers..."
-docker compose -f ./docker-compose-startup.yml stop
+docker-compose -f ./docker-compose-startup.yml stop
 
 :: Down and remove images
 echo "Removing docker containers and images"
-docker compose -f ./docker-compose-startup.yml down --rmi all
+docker-compose -f ./docker-compose-startup.yml down --rmi all
 
 :: launch specific docker compose (local) and remove orphans (previous containers that could have been removed from a docker compose)
 echo "Re-creating docker images and running containers"
-docker compose -f ./docker-compose-startup.yml up
+docker-compose -f ./docker-compose-startup.yml up
 exit
