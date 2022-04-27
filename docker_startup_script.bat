eaeb47465ef03e2b99c1c5390cceb3f9e81fd960
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-04-27 15:24:02 +0200

    Remove useless back to the line

--- docker_startup_script.bat
+++ docker_startup_script.bat
@@ -6,5 +6,4 @@ docker-compose -f ./docker-compose-local.yml down --rmi all
 
 echo "Re-creating docker images and running containers"
 docker-compose -f ./docker-compose-local.yml up --remove-orphans
-
 exit
