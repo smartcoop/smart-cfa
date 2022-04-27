a4bc41631ede756847f763e67082595b7b22018d
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-04-27 15:08:29 +0200

    Remove comments

--- docker_startup_script.bat
+++ docker_startup_script.bat
@@ -1,12 +1,9 @@
-:: Stop potentially running services
 echo "Stopping docker containers..."
 docker-compose stop
 
-:: Down and remove images
 echo "Removing docker containers and images"
 docker-compose -f ./docker-compose-local.yml down --rmi all
 
-:: launch specific docker compose (local) and remove orphans (previous containers that could have been removed from a docker compose)
 echo "Re-creating docker images and running containers"
 docker-compose -f ./docker-compose-local.yml up --remove-orphans
 
