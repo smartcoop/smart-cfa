6fd15d83f7c7c4ee1f6d1f55993ad65e7b572b48
Maxime Poulain <maxime.poulain@smart.coop>
2022-04-28 23:57:04 +0200

    Add migrations to generate super admin table

--- src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220428215638_Add_SuperAdmin_Table.Designer.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220428215638_Add_SuperAdmin_Table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Smart.FA.Catalog.Infrastructure.Persistence;
 
@@ -11,9 +12,10 @@ using Smart.FA.Catalog.Infrastructure.Persistence;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(CatalogContext))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220428215638_Add_SuperAdmin_Table")]
+    partial class Add_SuperAdmin_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
