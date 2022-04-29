84ab6cbf0a044eab9be3ec58e80a322f8d1325e9
Maxime Poulain <90202188+maxime-poulain@users.noreply.github.com>
2022-04-29 16:44:10 +0200

    Add support for super user rights (#94)
    This commit introduce a new table SuerUser that holds Smart user ids that should be considered as an user.

---
+++ src/UserAdmin/src/Smart.FA.Catalog.Application/UseCases/Queries/Authorization/IsSuperUserQuery.cs
@@ -0,0 +1,45 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Smart.FA.Catalog.Core.Extensions;
+using Smart.FA.Catalog.Infrastructure.Persistence;
+
+namespace Smart.FA.Catalog.Application.UseCases.Queries.Authorization;
+
+public class IsSuperUserQuery : IRequest<bool>
+{
+    public string UserId { get; }
+
+    public IsSuperUserQuery(string userId)
+    {
+        UserId = userId;
+    }
+}
+
+public class IsSuperUserQueryHandler : IRequestHandler<IsSuperUserQuery, bool>
+{
+    private readonly ILogger<IsSuperUserQueryHandler> _logger;
+    private readonly CatalogContext _catalogContext;
+
+    public IsSuperUserQueryHandler(ILogger<IsSuperUserQueryHandler> logger, CatalogContext catalogContext)
+    {
+        _logger = logger;
+        _catalogContext = catalogContext;
+    }
+
+    public async Task<bool> Handle(IsSuperUserQuery query, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await _catalogContext
+                .SuperUsers
+                .AsNoTracking()
+                .AnyAsync(superUser => superUser.UserId == query.UserId, cancellationToken: cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, $"Error handling `{query.GetType().Namespace}`. Query: {query.ToJson()}");
+            throw;
+        }
+    }
+}
