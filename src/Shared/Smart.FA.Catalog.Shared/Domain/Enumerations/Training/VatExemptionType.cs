e37754f7787506a8c776cabd064351ce1c9dd557
Maxime Poulain <maxime.poulain@smart.coop>
2022-05-02 14:58:53 +0200

    Add other value to enumeration VatExemptionType, TargetAudienceType and Topic

--- src/Shared/Smart.FA.Catalog.Shared/Domain/Enumerations/Training/VatExemptionType.cs
+++ src/Shared/Smart.FA.Catalog.Shared/Domain/Enumerations/Training/VatExemptionType.cs
@@ -2,9 +2,10 @@ namespace Smart.FA.Catalog.Shared.Domain.Enumerations.Training;
 
 public class VatExemptionType : Enumeration
 {
-    public static readonly VatExemptionType LanguageCourse = new(1, nameof(LanguageCourse));
-    public static readonly VatExemptionType Professional = new(2, nameof(Professional));
+    public static readonly VatExemptionType LanguageCourse  = new(1, nameof(LanguageCourse));
+    public static readonly VatExemptionType Professional    = new(2, nameof(Professional));
     public static readonly VatExemptionType ScholarTraining = new(3, nameof(ScholarTraining));
+    public static readonly VatExemptionType Other           = new(4, nameof(Other));
 
     private VatExemptionType(int id, string name) : base(id, name)
     {
