2c54baba41503ea5f9e5b433a282fee7dcb7dd19
Maxime Poulain <maxime.poulain@smart.coop>
2022-05-06 22:55:45 +0200

    Prevent invalid operation when approving an already approved user chart revision
    Approving an already approved user chart revision leads to a duplication of primary key of the many to many table between userchart revisions and trainers.
    Fixed that behavior by loading correctly the current trainer's user chart approvals and also redirect to the home page users that are either super user or having approved a active user chart revision.

--- src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/UserChart.cshtml.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/UserChart.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using MediatR;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Smart.FA.Catalog.Application.UseCases.Commands;
@@ -9,38 +9,63 @@ namespace Smart.FA.Catalog.Web.Pages;
 
 public class UserChartModel : PageModel
 {
-    private IMediator Mediator { get; }
+    private readonly IMediator _mediator;
 
-    public IUserIdentity UserIdentity { get; }
+    private readonly IUserIdentity _userIdentity;
 
-    [BindProperty] public bool HasAcceptedUserChart { get; set; } = false;
+    [BindProperty]
+    public bool HasAcceptedUserChart { get; set; } = false;
 
-    public ActionResult OnGet()
-    {
-        return Page();
-    }
+    public string LastChartUrl { get; private set; } = null!;
 
     public UserChartModel(IMediator mediator, IUserIdentity userIdentity)
     {
-        Mediator = mediator;
-        UserIdentity = userIdentity;
+        _mediator = mediator;
+        _userIdentity = userIdentity;
+    }
+
+    public async Task<ActionResult> OnGetAsync()
+    {
+        if (await ShouldReturnToHomePageAsync())
+        {
+            return RedirectToPage("/Admin/Index");
+        }
+
+        LastChartUrl = await GetLastChartUrlAsync();
+        return Page();
     }
 
     public async Task<ActionResult> OnPostAsync()
     {
-        if (HasAcceptedUserChart is false)
+        if (await ShouldReturnToHomePageAsync())
+        {
+            return RedirectToPage("/Admin/Index");
+        }
+
+        if (!HasAcceptedUserChart)
         {
             ModelState.AddModelError(string.Empty, CatalogResources.AdminHomePage_HasNotAcceptedUserChart);
             return Page();
         }
 
-        await Mediator.Send(new SetTrainerHasAcceptedLatestUserChartRequest { TrainerId = UserIdentity.Id });
+        await _mediator.Send(new SetTrainerHasAcceptedLatestUserChartRequest { TrainerId = _userIdentity.Id });
         return RedirectToPage("/Admin/Index");
     }
 
-    public async Task<string> GetLastChartUrl()
+    private async Task<bool> ShouldReturnToHomePageAsync()
     {
-        var response = await Mediator.Send(new GetLatestUserChartRevisionUrlRequest());
+        // There is no point to go any further if the user is either a Super User or if he has accepted already an active chart.
+        if (_userIdentity.IsSuperUser || await _mediator.Send(new HasAcceptedOneActiveUserChartRevisionQuery(_userIdentity.Id)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<string> GetLastChartUrlAsync()
+    {
+        var response = await _mediator.Send(new GetLatestUserChartRevisionUrlRequest());
 
         // If we ever find ourselves in a case where no user chart can't be retrieved from storage, we should display the base user chart in wwwroot (to avoid any legal conflict)
         // However it also means it should be updated regularly
