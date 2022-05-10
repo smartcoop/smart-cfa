8f6462a700f4a6f93e86da74acf7760cdbddb973
Victor-van-Duijnen <93646590+Victor-van-Duijnen@users.noreply.github.com>
2022-05-10 15:29:16 +0200

    Rewrite paging trainings to be SQL server 2008 compatible (#125)
    This PR introduces a hotfix for the paging system.
    
    FETCH/NEXT query commands are unsupported in sql Server 2008 and had to be replaced by a ROW_Number instruction for backward compatibility. There used to be a way to specifiy an options for forcing ef core to use the older SQL Server 2008 syntax but its support was dropped (read ref).
    
    - We found a nuget which allows us to have that same options in our sql set-up. This PR includes the nuget.
    - It also translates the previous query statement for the paging system to full ef core linq-to-sql query and an extension method to easily paginate a list.

---
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Extensions/PagedListExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Smart.FA.Catalog.Core.SeedWork;
+
+namespace Smart.FA.Catalog.Infrastructure.Extensions;
+
+public static class PagedListExtension
+{
+    public static async Task<PagedList<T>> PaginateAsync<T>(this IQueryable<T> source, PageItem pageItem)
+    {
+        var count = await source.CountAsync();
+        var items = await source.Skip(pageItem.Offset).Take(pageItem.PageSize).ToListAsync();
+        return new PagedList<T>(items, pageItem, count);
+    }
+}
