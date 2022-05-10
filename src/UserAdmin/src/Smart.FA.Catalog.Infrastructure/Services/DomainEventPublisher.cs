smartcoop/smart-cfa

commit 8f9eea743a6e717e0db7640703383294e0d3942c

Fix an issue where Domain Event could not be property published (#130)

src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Services/DomainEventPublisher.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Smart.FA.Catalog.Core.Extensions;
 using Smart.FA.Catalog.Core.LogEvents;
 using Smart.FA.Catalog.Core.SeedWork;
 
@@ -23,7 +24,7 @@ public class DomainEventPublisher : IDomainEventPublisher
         {
             foreach (var domainEvent in entity.DomainEvents)
             {
-                _logger.LogInformation(LogEventIds.DomainEventDispatch, $"Publishing domain event {domainEvent.GetType().Name} of entity {entity.GetType().Name} `{entity.Id}`.");
+                _logger.LogInformation(LogEventIds.DomainEventDispatch, $"Publishing Domain Event {domainEvent.GetType().Name} {domainEvent.ToJson()} of entity {entity.GetType().Name} `{entity.Id}`.");
                 await _publisher.Publish(domainEvent);
                 entity.RemoveDomainEvent(domainEvent);
             }

src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Persistence/CatalogContext.cs
@@ -75,7 +75,7 @@ public class CatalogContext : DbContext
         var entitiesWithEvents = ChangeTracker
             .Entries<Entity>()
             .Select(entry => entry.Entity)
-            .Where(entity => entity.DomainEvents.Count == 0);
+            .Where(entity => entity.DomainEvents.Any());
 
         return _eventPublisher.PublishEntitiesEventsAsync(entitiesWithEvents);
     }

commit c89e223502a974d973e9b1f6f53cda82dfcda445

Fix hidden error introduced with #130  (#134)
PR #130 fixes the resolution of Domain Events however there was another error under the hood while looping over the collection.
This commit fixes that.

src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Services/DomainEventPublisher.cs
@@ -26,9 +26,9 @@ public class DomainEventPublisher : IDomainEventPublisher
             {
                 _logger.LogInformation(LogEventIds.DomainEventDispatch, $"Publishing Domain Event {domainEvent.GetType().Name} {domainEvent.ToJson()} of entity {entity.GetType().Name} `{entity.Id}`.");
                 await _publisher.Publish(domainEvent);
-                entity.RemoveDomainEvent(domainEvent);
             }
 
+            entity.ClearDomainEvents();
             _logger.LogInformation(LogEventIds.DomainEventDispatch, $"Domain events publishing of entity {entity.GetType().Name} `{entity.Id}` completed with success.");
         }
     }