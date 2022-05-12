e19ee9b25a7b3417940b22161c5811f6bfad18e9
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-05-12 18:23:39 +0200

    Add missing account data service in account simulator

--- src/AccountSimulator/src/Smart.FA.Catalog.AccountSimulator/Pages/cfa.cshtml.cs
+++ src/AccountSimulator/src/Smart.FA.Catalog.AccountSimulator/Pages/cfa.cshtml.cs
@@ -4,9 +4,16 @@ namespace Smart.FA.Catalog.AccountSimulator.Pages;
 
 public class cfa : PageModel
 {
+    private readonly IAccountDataHeaderSerializer _accountDataHeaderSerializer;
+
+    public cfa(IAccountDataHeaderSerializer accountDataHeaderSerializer)
+    {
+        _accountDataHeaderSerializer = accountDataHeaderSerializer;
+    }
+
     public void OnGet(string? id)
     {
         Response.Cookies.Append("userid", id);
-        HttpContext.ProxyRedirect("/", id, AccountDataFactory.GetByUserId(id).Serialize());
+        HttpContext.ProxyRedirect("/", id, _accountDataHeaderSerializer.Serialize(_accountDataHeaderSerializer.GetByUserId(id)));
     }
 }
