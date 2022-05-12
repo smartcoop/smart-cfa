c8f1225d105da7405c6f604dce21804346e7edce
Maxime Poulain <90202188+maxime-poulain@users.noreply.github.com>
2022-05-12 19:56:54 +0200

    Update src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Mappers/TrainerMapper.cs
    Co-authored-by: Victor-van-Duijnen <93646590+Victor-van-Duijnen@users.noreply.github.com>

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Mappers/TrainerMapper.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Mappers/TrainerMapper.cs
@@ -21,6 +21,7 @@ public static class TrainerMapper
                 IconPathFileName = $"/icons/{socialNetworkName}.svg"
             });
         }
+        
         return socialNetworks;
     }
 
