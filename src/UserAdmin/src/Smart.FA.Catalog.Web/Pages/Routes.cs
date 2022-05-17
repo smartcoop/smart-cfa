9fb4863df0b163636fc4ffcf7138c9bb8e39bc98
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-05-17 12:50:23 +0200

    Change showcase accès membre redirection
    Also fix count of trainers

--- src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/Routes.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/Routes.cs
@@ -4,11 +4,11 @@ public static class Routes
 {
     public const string BasePath = "/cfa";
 
-    public const string HomePage = "/cfa/Admin/Index";
+    public const string HomePage = "/cfa/admin/index";
 
     public const string ErrorPage = "/cfa/500";
 
-    public const string UserChartApproval = "/cfa/UserChart";
+    public const string UserChartApproval = "/cfa/userChart";
 
     public const string SuperUserTrainingList = "/cfa/superuser/trainings";
 }
