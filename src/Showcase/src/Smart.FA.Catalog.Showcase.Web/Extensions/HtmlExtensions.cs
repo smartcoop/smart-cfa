e1968517d6c7846594006bf7296fceb39c622b2c
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-05-25 11:07:58 +0200

    Rewrite preserve line string extension as Htmlhelper extension

---
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Extensions/HtmlExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.FA.Catalog.Showcase.Web.Extensions;
+
+public static class StringHelper
+{
+    public static IHtmlContent PreserveLineBreaks(this IHtmlHelper input, string inputText)
+    {
+        inputText = inputText
+            .Replace("\r\n", "<br />")
+            .Replace("\n", "<br />");
+        return new HtmlString(inputText);
+    }
+}
