f7b0e161bf647ff440dfdd7c0e376d8df29c3e86
Joëlle <41791847+joe-coop@users.noreply.github.com>
2022-05-30 12:16:41 +0200

    Add popup contact in trainer card (#190)
    * Add js files from Mono Design
    
    * Add localization key
    
    * Add one more localization key
    
    * Add Mono js general file
    
    * Add some css to the link to the personal contact form
    
    * Add link to personal contact form and the html popup
    
    * Correct .js
    
    Co-authored-by: joe-ubik <joe@ubik.be>

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Localization/ShowcaseResources.Designer.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Localization/ShowcaseResources.Designer.cs
@@ -484,6 +484,15 @@ namespace Smart.FA.Catalog.Showcase.Localization {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string To {
+            get {
+                return ResourceManager.GetString("To", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Communication.
         /// </summary>
         public static string TopicName_Communication {
@@ -588,6 +597,15 @@ namespace Smart.FA.Catalog.Showcase.Localization {
         public static string ToTrainingHomePage {
             get {
                 return ResourceManager.GetString("ToTrainingHomePage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Envoyer un message.
+        /// </summary>
+        public static string TrainerCard_Contact {
+            get {
+                return ResourceManager.GetString("TrainerCard_Contact", resourceCulture);
             }
         }
         
