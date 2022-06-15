45f475f2041707a980340e262c7a52b832a55052
Maxime Poulain <maxime.poulain@smart.coop>
2022-06-15 13:02:41 +0200

    Add missing migration
    04d4f2f86dc3b90c829fd878a3e2d2a329524dfb fixed a bug with the ContextModelSnapshot.
    It did not include however a migration on auto generated SocialNetwork enumeration.
    This commits brings the missing data.

---
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220615105940_Update_SocialNetwork_Data.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Update_SocialNetwork_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Cfa",
+                table: "SocialNetwork",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "PersonalWebsite");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "Cfa",
+                table: "SocialNetwork",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Personal Website");
+        }
+    }
+}
