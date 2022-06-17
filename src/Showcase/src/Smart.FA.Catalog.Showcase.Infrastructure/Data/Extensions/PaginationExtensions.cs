245d82403743bd182d7d62bad52581f0726815c7
Maxime Poulain <90202188+maxime-poulain@users.noreply.github.com>
2022-06-17 10:16:47 +0200

    Apply randomization on showcase trainer and training list pages (#226)

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Infrastructure/Data/Extensions/PaginationExtensions.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Infrastructure/Data/Extensions/PaginationExtensions.cs
@@ -12,37 +12,35 @@ public static class PaginationExtensions
     /// <param name="query"><see cref="IQueryable{T}" /> on which the operation will be performed.</param>
     /// <param name="pageNumber">Current's page number.</param>
     /// <param name="pageSize">Number of item per page.</param>
-    /// <param name="randomIds">a boolean that indicates if the result queried from the data source should be randomly picked.</param>
+    /// <param name="randomizeIds">a boolean that indicates if the result queried from the data source should be randomly picked.</param>
     /// <returns>The paginated items and the total count.</returns>
     public static async Task<(List<T> PaginatedItems, int TotalCount)> PaginateAsync<T>(this IQueryable<T> query,
         int pageNumber,
         int pageSize,
-        bool randomIds = false)
+        bool randomizeIds = false)
         where T : IHasId
     {
         // Build a query that returns every entity ids.
         var idsQuery = query.Select(item => item.Id).Distinct();
+        if (randomizeIds)
+        {
+            idsQuery = idsQuery.RandomizeOrder();
+        }
 
-        // Proceed to compute the total.
-        var totalCount = await idsQuery.CountAsync();
+        var ids = await idsQuery.ToListAsync();
+        var totalCount = ids.Count;
 
         // Make sure to have the skip and take greater or equal to zero.
         // Negative values produce an exception
         var skip = Math.Max((pageNumber - 1) * pageSize, 0);
         var take = Math.Max(pageSize, 0);
 
-        if (randomIds)
-        {
-            idsQuery = idsQuery.RandomizeOrder();
-        }
-
-        var filteredIdsQuery = idsQuery.Skip(skip).Take(take);
-
+        // Applies pagination.
+        var filteredIdsQuery = ids.Skip(skip).Take(take);
         query = query.Where(item => filteredIdsQuery.Contains(item.Id));
 
-        // Make sure to keep consistent ordering.
-        var orderedList = await query.OrderBy(item => item.Id).ToListAsync();
-
-        return (orderedList, totalCount);
+        // Randomize again the result set coming from the randomized ids.
+        query = randomizeIds ? query.RandomizeOrder() : query.OrderBy(item => item.Id);
+        return (await query.ToListAsync(), totalCount);
     }
 }
