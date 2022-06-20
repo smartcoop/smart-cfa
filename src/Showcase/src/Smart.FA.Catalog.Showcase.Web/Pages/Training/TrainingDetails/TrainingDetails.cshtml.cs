98f290f2f919bc0ab818b67ed27ffa32c0e8f3e1
Maxime Poulain <maxime.poulain@smart.coop>
2022-06-20 12:23:35 +0200

    Showcase: fix translations when accessing trainings that does not exist

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Pages/Training/TrainingDetails/TrainingDetails.cshtml.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Pages/Training/TrainingDetails/TrainingDetails.cshtml.cs
@@ -1,12 +1,11 @@
 #nullable disable
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Smart.FA.Catalog.Shared.Domain.Enumerations.Training;
 
 namespace Smart.FA.Catalog.Showcase.Web.Pages.Training.TrainingDetails;
 
-public class TrainingDetailsModel : PageModel
+public class TrainingDetailsModel : PageModelBase
 {
     private readonly Infrastructure.Data.CatalogShowcaseContext _context;
 
@@ -21,18 +20,14 @@ public class TrainingDetailsModel : PageModel
     {
         if (id == null)
         {
-            //TO DO: add translated custom messages
-            TempData["errorMessage"] = "There is no record for this request. Please try again.";
-            return RedirectToPage("/404");
+            return RedirectToNotFound();
         }
 
         var trainingDetails = await _context.TrainingDetails.Where(training => training.Id == id).ToListAsync();
 
         if (!trainingDetails.Any())
         {
-            //TO DO: add translated custom messages
-            TempData["errorMessage"] = "This training does not exist! Please try again.";
-            return RedirectToPage("/404");
+            return RedirectToNotFound(message: ShowcaseResources.TrainingWasNotFound);
         }
 
         Training = MapTrainingDetails(trainingDetails);
