dc9ad2daf4d92e5812bd1a5ac5a78941824c73b5
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-06-28 15:38:04 +0200

    Relocate and respecify paging extension method

--- src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/Shared/Components/PagingTile/PagingTile.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/Shared/Components/PagingTile/PagingTile.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Smart.FA.Catalog.Core.SeedWork;
 using Smart.FA.Catalog.Shared.Collections;
 
 namespace Smart.FA.Catalog.Web.Pages.Shared.Components.PagingTile;
 
 public class PagingTile : ViewComponent
 {
-    public IViewComponentResult Invoke(PagedList<object> list, string route)
+    public IViewComponentResult Invoke(PagedList<Entity> list, string route)
     {
         return View(new PagingObject { List = list, Route = route });
     }
@@ -13,6 +14,6 @@ public class PagingTile : ViewComponent
 
 public class PagingObject
 {
-    public PagedList<object> List { get; set; }
+    public PagedList<Entity> List { get; set; }
     public string Route { get; set; }
 }
