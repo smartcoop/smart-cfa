d32a1261186f7f709a656bf0ce418869dfa173db
Maxime Poulain <maxime.poulain@smart.coop>
2022-06-28 11:12:09 +0200

    Add a bunch of rules to validate min and max HTML inner text

--- src/UserAdmin/src/Smart.FA.Catalog.Application/Extensions/FluentValidation/OnStringRuleBuilderExtensions.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Application/Extensions/FluentValidation/OnStringRuleBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using HtmlAgilityPack;
 using Smart.FA.Catalog.Shared.Collections;
 
 namespace Smart.FA.Catalog.Application.Extensions.FluentValidation;
@@ -17,5 +18,53 @@ public static class OnStringRuleBuilderExtensions
         return ruleBuilder
             .Must(email => EmailValidator.Validate(email))
             .WithMessage(CatalogResources.InvalidEmail);
+    }
+
+    /// <summary>
+    /// Defines a length validator on the current rule builder, but only for string properties that are HTML.
+    /// Validation will fail if the length of the HTML inner text is less than the length specified.
+    /// </summary>
+    /// <typeparam name="T">Type of object being validated</typeparam>
+    /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+    /// <param name="minimumLength"></param>
+    /// <returns></returns>
+    public static IRuleBuilderOptions<T, string?> MinimumHtmlInnerLength<T>(this IRuleBuilder<T, string?> ruleBuilder, int minimumLength)
+    {
+        return ruleBuilder.Must(html =>
+        {
+            if (html is null)
+            {
+                return true;
+            }
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            return htmlDocument.DocumentNode.InnerText.Length >= minimumLength;
+        });
+    }
+
+    /// <summary>
+    /// Defines a length validator on the current rule builder, but only for string properties that are HTML.
+    /// Validation will fail if the length of the HTML's inner text is larger than the length specified.
+    /// </summary>
+    /// <typeparam name="T">Type of object being validated</typeparam>
+    /// <param name="ruleBuilder">The rule builder on which the validator should be defined</param>
+    /// <param name="maximumLength"></param>
+    /// <returns></returns>
+    public static IRuleBuilderOptions<T, string?> MaximumHtmlInnerLength<T>(this IRuleBuilder<T, string?> ruleBuilder, int maximumLength)
+    {
+        return ruleBuilder.Must(html =>
+        {
+            if (html is null)
+            {
+                return true;
+            }
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            return htmlDocument.DocumentNode.InnerText.Length <= maximumLength;
+        });
     }
 }
