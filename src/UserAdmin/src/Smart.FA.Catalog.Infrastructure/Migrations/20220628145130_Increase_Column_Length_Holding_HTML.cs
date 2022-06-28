0f9813e3830e907cdeb22d040bb35205d28526c7
Maxime Poulain <maxime.poulain@smart.coop>
2022-06-28 16:51:56 +0200

    Add migrations

---
+++ src/UserAdmin/src/Smart.FA.Catalog.Infrastructure/Migrations/20220628145130_Increase_Column_Length_Holding_HTML.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Increase_Column_Length_Holding_HTML : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PracticalModalities",
+                schema: "Cfa",
+                table: "TrainingLocalizedDetails",
+                type: "nvarchar(1500)",
+                maxLength: 1500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Methodology",
+                schema: "Cfa",
+                table: "TrainingLocalizedDetails",
+                type: "nvarchar(1500)",
+                maxLength: 1500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Goal",
+                schema: "Cfa",
+                table: "TrainingLocalizedDetails",
+                type: "nvarchar(1500)",
+                maxLength: 1500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Biography",
+                schema: "Cfa",
+                table: "Trainer",
+                type: "nvarchar(2250)",
+                maxLength: 2250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1500)",
+                oldMaxLength: 1500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PracticalModalities",
+                schema: "Cfa",
+                table: "TrainingLocalizedDetails",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1500)",
+                oldMaxLength: 1500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Methodology",
+                schema: "Cfa",
+                table: "TrainingLocalizedDetails",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1500)",
+                oldMaxLength: 1500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Goal",
+                schema: "Cfa",
+                table: "TrainingLocalizedDetails",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1500)",
+                oldMaxLength: 1500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Biography",
+                schema: "Cfa",
+                table: "Trainer",
+                type: "nvarchar(1500)",
+                maxLength: 1500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2250)",
+                oldMaxLength: 2250);
+        }
+    }
+}
