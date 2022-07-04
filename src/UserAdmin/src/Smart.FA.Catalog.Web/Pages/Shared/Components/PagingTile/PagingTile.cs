65293d7ec1433e483d8e49c92689cdddb1d8a482
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-07-04 10:46:34 +0200

    Use smart-pagination tag helper in pagination tile

--- src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/Shared/Components/PagingTile/PagingTile.cs
+++ src/UserAdmin/src/Smart.FA.Catalog.Web/Pages/Shared/Components/PagingTile/PagingTile.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Smart.Design.Razor.TagHelpers.Pagination;
 using Smart.FA.Catalog.Core.SeedWork;
 using Smart.FA.Catalog.Shared.Collections;
 
@@ -6,14 +7,41 @@ namespace Smart.FA.Catalog.Web.Pages.Shared.Components.PagingTile;
 
 public class PagingTile : ViewComponent
 {
-    public IViewComponentResult Invoke(PagedList<Entity> list, string route)
+    public IViewComponentResult Invoke(PagedList<Entity> list, Dictionary<string,string?> queryParameters)
     {
-        return View(new PagingObject { List = list, Route = route });
+        var paginationSettings = GetPaginationSettings(list, GetQueryParametersAsString(queryParameters));
+        return View(new PagingObject { List = list, PaginationSettings = paginationSettings });
+    }
+
+    private string GetQueryParametersAsString(Dictionary<string, string?> queryParameters)
+    {
+        var queryParametersString = string.Empty;
+        foreach (var queryParameter in queryParameters)
+        {
+            if (!string.IsNullOrEmpty(queryParameter.Value))
+            {
+                queryParametersString += $"{queryParameter.Key}={queryParameter.Value}";
+            }
+        }
+
+        return queryParametersString;
+    }
+
+    private PaginationSettings GetPaginationSettings(PagedList<Entity> list, string? searchKeyword)
+    {
+        return new PaginationSettings
+        {
+            NumberOfLinks = 5,
+            PageNumber = list.CurrentPage,
+            PageNumberParameterName = nameof(list.CurrentPage),
+            TotalPages = list.TotalPages,
+            QueryString = searchKeyword
+        };
     }
 }
 
 public class PagingObject
 {
     public PagedList<Entity> List { get; set; }
-    public string Route { get; set; }
+    public PaginationSettings PaginationSettings { get; set; }
 }
