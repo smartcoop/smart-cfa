3ef5e221edb9277cfb1759a7688b320c4efc0c54
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-07-18 14:44:34 +0200

    Remove minio-server from docker-compose and trigger minio-client on static instance

--- pipeline-scripts/jenkins-deploy.sh
+++ pipeline-scripts/jenkins-deploy.sh
@@ -2,19 +2,21 @@
 
 # This scripts is called by Jenkins whenever a branch is pushed to GitHub.
 
+# Exit immediately from script if any command send back an error code
 set -e
 
-# Convert uppercase to lowercase.
-DOCKER_NAME=$(echo cfa-${BRANCH_NAME} | sed -e 's/\(.*\)/\L\1/' -e 's/origin\/master/master/g' -e 's/origin\/feature\//feature-/g' -e 's/origin\/hotfix\//hotfix-/g' -e 's/feature\//feature-/g' -e 's/hotfix\//hotfix-/g')
-DOCKER_HOSTNAME=$(echo ${DOCKER_NAME})
+# Set-up docker container's name
+DOCKER_NAME=$(echo cfa-"${BRANCH_NAME}" | sed -e 's/\(.*\)/\L\1/' -e 's/origin\/master/master/g' -e 's/origin\/feature\//feature-/g' -e 's/origin\/hotfix\//hotfix-/g' -e 's/feature\//feature-/g' -e 's/hotfix\//hotfix-/g')
+DOCKER_HOSTNAME=${DOCKER_NAME}
 
+# Exit without deploying if the branch's name use non-authorized character or if the branch is a PR branch
 echo
-if ! echo ${DOCKER_NAME} |  grep -q "^[a-z][a-z0-9_-]*$"; then
+if ! echo "${DOCKER_NAME}" |  grep -q "^(?!pr).+[a-z][a-z0-9_-]*$"; then
   echo "The branch does not use only alphanumeric characters and dashes. Exiting.";
   exit 1
 fi
 
-# TODO HTML-escape those strings, especially the subject.
+# Create variables for tracking the metadata of the deploy
 GIT_COMMIT="$(git rev-parse HEAD)"
 GIT_COMMIT_SUBJECT="$(git log -n1 --format=format:"%s")"
 GIT_COMMIT_DATE="$(git log -n1 --format=format:"%ci")"
@@ -31,21 +33,26 @@ echo "Git date: ${GIT_COMMIT_DATE}"
 echo "Git author name: ${GIT_AUTHOR_NAME}"
 echo "Git author email: ${GIT_AUTHOR_EMAIL}"
 
-echo "DOCKER_NAME=${DOCKER_NAME}" > .env
-echo "DOCKER_SQL_USER=${USERNAME}" >> .env
-echo "DOCKER_SQL_PASSWORD=${PASSWORD}" >> .env
-echo "DOCKER_ENVIRONMENT=Staging" >> .env
-echo "MINIO_ROOT_USER=${DOCKER_MINIO_USER}" >> .env
-echo "MINIO_ROOT_PASSWORD=${DOCKER_MINIO_PASSWORD}" >> .env
+{
+    echo "DOCKER_NAME=${DOCKER_NAME}"
+    echo "DOCKER_SQL_USER=${USERNAME}"
+    echo "DOCKER_SQL_PASSWORD=${PASSWORD}"
+    echo "DOCKER_ENVIRONMENT=Staging"
+    echo "MINIO_ROOT_USER=${DOCKER_MINIO_USER}"
+    echo "MINIO_ROOT_PASSWORD=${DOCKER_MINIO_PASSWORD}"
+} >> .env
 
+# Swap in all placeholders with sensitive or contextual data in the appsettings and make a tmp copy from it
 sed -e "s/{catalog-server-name}/${DOCKER_NAME}-datasource/" \
     -e "s/{catalog-server-user-id}/$USERNAME/" \
     -e "s/{catalog-server-user-password}/$PASSWORD/" \
     -e "s/{docker_name-minio}/${DOCKER_NAME}-minio/" \
     ./src/UserAdmin/src/Smart.FA.Catalog.Web/appsettings.Staging.json > ./src/UserAdmin/src/Smart.FA.Catalog.Web/appsettings.Staging.tmp.json
 
+# Replace the actual file with the tmp copy
 mv ./src/UserAdmin/src/Smart.FA.Catalog.Web/appsettings.Staging.tmp.json ./src/UserAdmin/src/Smart.FA.Catalog.Web/appsettings.Staging.json
 
+# If a SQL server container is not already running for cfa, then we create it
 if [ "$( docker container inspect -f '{{.State.Running}}' "dev-cfa-datasource" )" != "true" ]; then
 
 echo "dev-cfa-datasource not running"
@@ -66,7 +73,7 @@ else
   echo "dev-cfa-datasource running"
 fi
 
-
+# If a Minio server container is not already running for cfa, then we create it
 if [ "$( docker container inspect -f '{{.State.Running}}' "dev-cfa-minio" )" != "true" ]; then
 
 echo "dev-cfa-minio not running"
@@ -88,8 +95,8 @@ else
   echo "dev-cfa-minio running"
 fi
 
-
-echo "build docker DB and API"
+# Use the docker compose file to run all other containers while passing environment variables
+echo "build docker Web UserAdmin and Showcase app"
 docker-compose --env-file ./.env build
 echo "stop old docker"
 docker-compose down || true
