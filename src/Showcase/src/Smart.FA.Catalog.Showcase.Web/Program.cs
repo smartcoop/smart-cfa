a083f8595fef832adf5f688f3645165dd3eb695b
Victor-van-Duijnen <victor.van.duijnen@smart.coop>
2022-07-22 09:41:54 +0200

    Add templated local appsettings for easy local debug setup
    Set-up is created for both useradmin and showcase

--- src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Program.cs
+++ src/Showcase/src/Smart.FA.Catalog.Showcase.Web/Program.cs
@@ -5,6 +5,10 @@ using Smart.FA.Catalog.Showcase.Web.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// appsettings.Local.json will have precedence over anything else as it is set in last.
+// https://github.com/dotnet/aspnetcore/blob/c5207d21ed68041879e1256406b458d130b420ab/src/DefaultBuilder/src/WebHost.cs#L170
+builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
 // Use NLog as logging provider
 builder.Host.UseNLog();
 
